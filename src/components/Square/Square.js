da36170831f0e91b622360a244870e32bbdb9145
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-04-07 01:33:36 -0400

    Align damn it

--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -5,7 +5,8 @@ class Square extends Component {
   render() {
     return (
       <div className="Square">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Xiangqi_black_side_General.svg/50px-Xiangqi_black_side_General.svg.png" />
+        <div className="Piece">
+        </div>
       </div>
     );
   }
