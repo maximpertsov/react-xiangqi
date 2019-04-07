79f3d7364078a460e560ea795b171c3f09c9c803
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-04-07 00:18:53 -0400

    Spike board

---
+++ src/components/Board/Board.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './Board.css';
+
+class Board extends Component {
+  render() {
+    return (
+      <div className="Board">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/6/66/Xiangqi-_md.PNG" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/6/66/Xiangqi-_md.PNG" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/6/66/Xiangqi-_md.PNG" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/6/66/Xiangqi-_md.PNG" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/6/66/Xiangqi-_md.PNG" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/6/66/Xiangqi-_md.PNG" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/6/66/Xiangqi-_md.PNG" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/6/66/Xiangqi-_md.PNG" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/6/66/Xiangqi-_md.PNG" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/6/66/Xiangqi-_md.PNG" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/6/66/Xiangqi-_md.PNG" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/6/66/Xiangqi-_md.PNG" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/6/66/Xiangqi-_md.PNG" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/6/66/Xiangqi-_md.PNG" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/6/66/Xiangqi-_md.PNG" />
+      </div>
+    );
+  }
+}
+
+export default Board;
