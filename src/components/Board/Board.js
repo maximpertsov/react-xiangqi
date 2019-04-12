c09db127f6ed67ff192293eb5fa190e22c7c617a
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-04-12 11:43:59 -0400

    pieces on first two rows

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,12 +1,17 @@
+/* eslint no-bitwise: ["error", { "allow": ["~"] }] */
+
 import React, { Component } from 'react';
 import './Board.css';
 import Square from '../Square/Square';
 
 class Board extends Component {
   render() {
+    const pieceImgSrc = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Xiangqi_black_side_General.svg/50px-Xiangqi_black_side_General.svg.png';
+    const showImg = (i) => [0, 1, 8, 9].includes(~~(i / 9));
+
     return (
       <div className="Board">
-        {[...Array(90)].map(() => <Square />)}
+        {[...Array(90)].map((x, i) => <Square pieceImgSrc={showImg(i) ? pieceImgSrc : ""} />)}
       </div>
     );
   }
