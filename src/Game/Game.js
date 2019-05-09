a536a3aec4bbb29d2fdd28acb608a2260071d27a
maximpertsov <maxim.pertsov@gmail.com>
2019-05-09 18:54:47 -0400

    Refactor game logic (#2)
    * Add-only refactor
    
    * Move change player logic up
    
    * Use node version 10.10

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -1,19 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 import Board from './Board/Board';
 import Player from './Player/Player';
+
+const redPlayer = <Player color="red" />;
+
+const blackPlayer = <Player color="black" />;
 
 const Wrapper = styled.div`
   text-align: center;
 `;
 
-const Game = () => (
-  <Wrapper className="Game">
-    <Board
-      redPlayer={<Player color="red" />}
-      blackPlayer={<Player color="black" />}
-    />
-  </Wrapper>
-);
+class Game extends Component {
+  constructor(props) {
+    super(props);
+    const { players } = this.props;
+
+    this.activePlayer = this.activePlayer.bind(this);
+    this.changePlayer = this.changePlayer.bind(this);
+
+    this.state = {
+      activePlayerIdx: 0,
+      players,
+    };
+  }
+
+  // TODO: create PlayerManager class?
+  activePlayer() {
+    const { players, activePlayerIdx } = this.state;
+    return players[activePlayerIdx];
+  }
+
+  // TODO: create PlayerManager class?
+  changePlayer() {
+    const { players } = this.state;
+    this.setState((prevState) => ({
+      activePlayerIdx: (prevState.activePlayerIdx + 1) % players.length,
+    }));
+  }
+
+  render() {
+    const { players } = this.state;
+    return (
+      <Wrapper className="Game">
+        <Board
+          redPlayer={players[0]}
+          blackPlayer={players[1]}
+          activePlayer={this.activePlayer}
+          changePlayer={this.changePlayer}
+        />
+      </Wrapper>
+    );
+  }
+}
+
+Game.propTypes = {
+  players: PropTypes.arrayOf(PropTypes.element),
+};
+
+Game.defaultProps = {
+  players: [redPlayer, blackPlayer],
+};
 
 export default Game;
