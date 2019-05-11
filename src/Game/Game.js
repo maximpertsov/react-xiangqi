191f7bea1677fbd329aaf63b9ef858a38be98fd2
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-05-11 00:25:22 -0400

    Add GameInfo

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -1,13 +1,8 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 import Board from './Board/Board';
-import Player from './Player/Player';
+import GameInfo from './GameInfo';
 import { getGame } from '../client';
-
-const redPlayer = <Player color="red" />;
-
-const blackPlayer = <Player color="black" />;
 
 const Wrapper = styled.div`
   text-align: center;
@@ -16,23 +11,19 @@ const Wrapper = styled.div`
 class Game extends Component {
   constructor(props) {
     super(props);
-    const { players } = this.props;
 
     this.activePlayer = this.activePlayer.bind(this);
     this.changePlayer = this.changePlayer.bind(this);
 
     this.state = {
       activePlayerIdx: 0,
-      players,
+      players: [],
     };
   }
 
   componentDidMount() {
     getGame(1).then((data) => {
-      const players = data.players.map(
-        (player) => <Player name={player.name} color={player.color} />,
-      );
-      this.setState({ players });
+      this.setState({ players: data.players });
     });
   }
 
@@ -54,23 +45,17 @@ class Game extends Component {
     const { players } = this.state;
     return (
       <Wrapper className="Game">
-        <Board
+        <GameInfo
           redPlayer={players[0]}
           blackPlayer={players[1]}
           activePlayer={this.activePlayer}
+        />
+        <Board
           changePlayer={this.changePlayer}
         />
       </Wrapper>
     );
   }
 }
-
-Game.propTypes = {
-  players: PropTypes.arrayOf(PropTypes.element),
-};
-
-Game.defaultProps = {
-  players: [redPlayer, blackPlayer],
-};
 
 export default Game;
---
+++ src/Game/GameInfo.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const GameInfo = ({ activePlayer, redPlayer, blackPlayer }) => {
+  if (redPlayer === undefined || blackPlayer === undefined) {
+    return (<div><p>Loading...</p></div>);
+  }
+  const { color } = activePlayer();
+  return (
+    <div>
+      <p>
+        { `${redPlayer.name} [red] vs ${blackPlayer.name} [black]` }
+      </p>
+      <p>
+        { `${color}'s turn` }
+      </p>
+    </div>
+  );
+};
+
+const playerShape = PropTypes.shape({
+  color: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+});
+
+GameInfo.propTypes = {
+  activePlayer: PropTypes.func.isRequired,
+  blackPlayer: playerShape.isRequired,
+  redPlayer: playerShape.isRequired,
+};
+
+export default GameInfo;
