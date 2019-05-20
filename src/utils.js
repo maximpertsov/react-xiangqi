b4dec4c4907e6cf8800bd18247387359db569033
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-05-20 08:34:05 -0400

    WIP: spike legal moves

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,16 @@
 const RANKS = 10;
 const FILES = 9;
+const RED_PIECES = "RHEAKCP";
+const BLACK_PIECES = "rheakcp";
 
 export const getIndex = (rank, file) => file + rank * FILES;
-export const getRank = (i) => Math.ceil(i / RANKS);
-export const getFile = (i) => i % FILES;
+export const getRank = (idx) => Math.ceil(idx / RANKS);
+export const getFile = (idx) => idx % FILES;
+const isRed = (code) => RED_PIECES.includes(code);
+const isBlack = (code) => BLACK_PIECES.includes(code);
+function sameColor(code1, code2) {
+  return (isRed(code1) && isRed(code2)) || (isBlack(code1) && isBlack(code2))
+}
 
 function fromFenRow(row) {
   return row.split('').reduce((acc, ch) => {
@@ -15,6 +22,14 @@ function fromFenRow(row) {
 
 export function fromFen(fen) {
   return fen.split('/').reduce((acc, row) => acc.concat(fromFenRow(row)), []);
+}
+
+function isUniverallyLegal(moves, board, fromIdx, toIdx) {
+  if (fromIdx === toIdx) return false;
+  if (toIdx < 0) return false;
+  if (toIdx >= RANKS * FILES) return false;
+  if (sameColor(board[fromIdx], board[toIdx])) return false;
+  return true;
 }
 
 export function legalMoves(board) {
