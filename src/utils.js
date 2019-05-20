b50a8f417f564e6e4a72fed4c3f0ff3113837dc9
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-05-19 22:43:55 -0400

    Define fromFen

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,13 @@
+function fromFenRow(row) {
+  return row.split('').reduce((acc, ch) => {
+    const val = +ch;
+    const newItems = Number.isNaN(val) ? [ch] : Array(val).fill(null);
+    return acc.concat(newItems);
+  }, []);
+}
+
 export function fromFen(fen) {
-  return fen;
+  return fen.split('/').map((row) => fromFenRow(row));
 }
 
 export default {};
