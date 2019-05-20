b7c9ec0c827e1157166c97a8cdcb4b7121a84554
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-05-20 07:02:36 -0400

    Fix

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,13 @@
 function fromFenRow(row) {
   return row.split('').reduce((acc, ch) => {
     const val = +ch;
-    const newItems = Number.isNaN(val) ? [ch] : Array(val).fill(null);
+    const newItems = Number.isNaN(val) ? [ch] : [...Array(val).fill(null)];
     return acc.concat(newItems);
   }, []);
 }
 
 export function fromFen(fen) {
-  return fen.split('/').reduce((acc, row) => acc.concat(fromFenRow(row)));
+  return fen.split('/').reduce((acc, row) => acc.concat(fromFenRow(row)), []);
 }
 
 export default {};
--- src/utils.test.js
+++ src/utils.test.js
@@ -1,9 +1,9 @@
 import { fromFen } from './utils';
 
-const RANKS = 8;
+const RANKS = 10;
 const FILES = 9;
 
-test('converts FEN string to a 2d array', () => {
+test('converts FEN string to an array', () => {
   const fen = 'rheakaehr/9/2c3c2/p1p1p1p1p/9/9/P1P1P1P1P/2C3C2/9/RHEAKAEHR';
   const expected = [
     'r', 'h', 'e', 'a', 'k', 'a', 'e', 'h', 'r',
@@ -18,9 +18,7 @@ test('converts FEN string to a 2d array', () => {
     'R', 'H', 'E', 'A', 'K', 'A', 'E', 'H', 'R',
   ];
   const actual = fromFen(fen);
-  for (let rank = 0; rank < RANKS; rank++) {
-    for (let file = 0; file < FILES; file++) {
-      expect(actual[rank][file]).toBe(expected[rank][file]);
-    }
+  for (let i = 0; i < RANKS * FILES; i++) {
+    expect(actual[i]).toBe(expected[i]);
   }
 });
