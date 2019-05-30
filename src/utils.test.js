b904d1813e37ce47b5d7a73f59374a1fc609d2ac
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-05-30 10:08:31 -0400

    Add advanced horse position

--- src/utils.test.js
+++ src/utils.test.js
@@ -180,6 +180,28 @@ const legalMoveTests = [
       [getSlot(8, 8)]: toSlots([9, 8], [8, 7]),
     },
   ],
+  // Bugfix: Missing horse positions
+  [
+    'rheakaehr/9/1c5c1/p1p1p1p1p/9/2P6/P3P1P1P/1CH4C1/9/R1EAKAEHR',
+    {
+      // pawn moves
+      [getSlot(3, 0)]: toSlots([4, 0]),
+      [getSlot(3, 2)]: toSlots([4, 2]),
+      [getSlot(3, 4)]: toSlots([4, 4]),
+      [getSlot(3, 6)]: toSlots([4, 6]),
+      [getSlot(3, 8)]: toSlots([4, 8]),
+      [getSlot(6, 0)]: toSlots([5, 0]),
+      [getSlot(5, 2)]: toSlots([4, 2]),
+      [getSlot(6, 4)]: toSlots([5, 4]),
+      [getSlot(6, 6)]: toSlots([5, 6]),
+      [getSlot(6, 8)]: toSlots([5, 8]),
+      // horse moves
+      [getSlot(0, 1)]: toSlots([2, 0], [2, 2]),
+      [getSlot(0, 7)]: toSlots([2, 6], [2, 8]),
+      [getSlot(7, 2)]: toSlots([9, 1], [8, 4], [5, 1], [5, 3]),
+      [getSlot(9, 7)]: toSlots([7, 6], [7, 8]),
+    },
+  ],
 ];
 
 test.each(legalMoveTests)('finds all legal moves for %s', (fen, expected) => {
