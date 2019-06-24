7faa1e0c5b188a4643eb63fa30553b8794016a65
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-06-24 01:11:49 -0400

    Sorta algebraic notation

--- src/Game/Move/Move.js
+++ src/Game/Move/Move.js
@@ -3,28 +3,58 @@ import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 
 const Wrapper = styled.div(
-  ({ selected }) => ({
-    backgroundColor: (selected ? 'grey' : 'none'),
+  ({ selected, piece }) => ({
+    // TODO: find a better selction color
+    backgroundColor: (selected ? '#abc999' : 'none'),
+    color: ('RHEAKCP'.includes(piece) ? 'red' : 'black'),
   }),
 );
+
+// TODO: remove temporary lookups in favor of something better?
+const F = 'abcdefghi';
+const R = Array(10).fill().map((x, i) => i + 1).reverse();
+const P = {
+  p: '卒',
+  P: '兵',
+  r: '車',
+  R: '車',
+  h: '馬',
+  H: '馬',
+  e: '象',
+  E: '相',
+  a: '士',
+  A: '仕',
+  k: '將',
+  K: '帥',
+  c: '砲',
+  C: '炮',
+};
+
+const moveString = ({ piece, fromPos, toPos }) => {
+  const [fromRank, fromFile] = fromPos;
+  const [toRank, toFile] = toPos;
+  return `${P[piece]}${F[fromFile]}${R[fromRank]}-${F[toFile]}${R[toRank]}`;
+};
 
 const Move = ({
   fromPos, toPos, piece, handleMoveSelect, order, selected,
 }) => {
   if (piece === null) return null;
 
-  const description = `${piece}: ${fromPos} -> ${toPos}`;
+  const description = moveString({ piece, fromPos, toPos });
   return (
     <Wrapper
       className="Move"
       onClick={(e) => handleMoveSelect(e, order)}
       selected={selected}
+      piece={piece}
     >
       <p>{ description }</p>
     </Wrapper>
   );
 };
 
+// TODO: allow fromPos, toPos, piece to be null
 Move.propTypes = {
   handleMoveSelect: PropTypes.func.isRequired,
   fromPos: PropTypes.arrayOf(PropTypes.number).isRequired,
