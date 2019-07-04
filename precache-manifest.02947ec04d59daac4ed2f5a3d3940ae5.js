4864b573cab665efe2557fd86b5acb8e2fdd7f6a
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-07-04 01:37:45 -0400

    Updates

---
+++ precache-manifest.02947ec04d59daac4ed2f5a3d3940ae5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cd7218ff408c5be685dc155e6477fe5",
+    "url": "/react-xiangqi/static/media/board-1000px.svg.4cd7218f.png"
+  },
+  {
+    "revision": "a8f5a97ac54f95256a4d",
+    "url": "/react-xiangqi/static/js/runtime~main.fab9e7fb.js"
+  },
+  {
+    "revision": "029148caefd00ad48173",
+    "url": "/react-xiangqi/static/js/main.4f642d64.chunk.js"
+  },
+  {
+    "revision": "fdba914923563cbb4019",
+    "url": "/react-xiangqi/static/js/2.5786b6fe.chunk.js"
+  },
+  {
+    "revision": "7e8b9fa340f643686429240631ec7dd0",
+    "url": "/react-xiangqi/index.html"
+  }
+];
