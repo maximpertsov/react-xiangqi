a1d581733a8b82ca67b60b4efd0518bff6edba84
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-07-04 13:47:56 -0400

    Updates

--- precache-manifest.50352cbe05527e218f1b951cd522b888.js
+++ precache-manifest.50352cbe05527e218f1b951cd522b888.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "f2e93b3ed566d6ea415a",
-    "url": "/static/js/main.8b67b91f.chunk.js"
+    "revision": "ea1149c6e1bc7692e0a5",
+    "url": "/static/js/main.9ee2418a.chunk.js"
   },
   {
     "revision": "454d259743579827cd61",
     "url": "/static/js/2.14da3d22.chunk.js"
   },
   {
-    "revision": "a7dcbff1a0b55433c3a63d624462972b",
+    "revision": "72f30076bd5f2a70109151208a5e2dad",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.40dbd7a15596499e5f17565c6cb4611d.js"
+  "/precache-manifest.50352cbe05527e218f1b951cd522b888.js"
 );
 
 workbox.clientsClaim();
