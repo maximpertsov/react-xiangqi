1f831b8ab162460f1fa11b040ad36d002e085323
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-07-08 00:00:39 -0400

    Spike MobX store

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { observable, decorate } from 'mobx';
 import Game from './Game/Game';
 import 'babel-polyfill';
 
 // const GAME_ID = 'ABC123';
 
+class Store {
+  value = 0;
+}
+
+decorate(Store, {
+  value: observable,
+});
+
+const store = new Store();
+
 const App = () => {
   const queryParams = new URLSearchParams(window.location.search);
   const gameSlug = queryParams.get('game') || undefined;
-  return <Game gameSlug={gameSlug} />;
+  return <Game store={store} gameSlug={gameSlug} />;
 };
 
 export default App;
