09a0a694bf8f487541b5bb3f3e6fde1d9886df47
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-07-13 17:26:33 -0400

    Revert "WIP: SquareWithPiece and EmptySquare"
    This reverts commit 3c3dcfffbaf67f03facaca654d4240b9ccee1c50.

--- src/Game/Square/Square.js
+++ src/Game/Square/Square.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import EmptySquare from './EmptySquare';
-import SquareWithPiece from './SquareWithPiece';
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
 
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+import XiangqiPiece from '../Piece/Piece';
+
+const SELECTION_GREEN = 'rgba(30, 179, 0, 0.3)';
+const IN_CHECK_RED = 'red';
+
+const TargetedEmptySquare = styled.div`
+  width:50%;
+  height:50%;
+  position:relative;
+  top:50%;
+  transform:translateY(-50%);
+  border-radius:50%;
+  background:${SELECTION_GREEN};
+`;
 
 const Square = ({
   handleSquareClick,
@@ -14,24 +28,45 @@ const Square = ({
 }) => {
   const isOccupied = () => pieceCode !== undefined;
 
+  const renderSquareElement = () => {
+    if (isOccupied()) return <XiangqiPiece code={pieceCode} />;
+    if (targeted) return (<TargetedEmptySquare />);
+    return (<div />);
+  };
+
+  const attacked = () => isOccupied() && targeted;
+
+  const inCheck = () => slot === inCheckSlot;
+
   const handleClick = () => {
     handleSquareClick(slot);
   };
 
-
-  if (!isOccupied()) {
-    return <EmptySquare handleClick={handleClick} targeted={targeted} />;
-  }
+  const getOutline = () => {
+    if (attacked()) return `2px dotted ${SELECTION_GREEN}`;
+    if (inCheck()) return `2px dotted ${IN_CHECK_RED}`;
+    return 'none';
+  };
 
   return (
-    <SquareWithPiece
-      handleClick={handleClick}
-      pieceCode={pieceCode}
-      slot={slot}
-      selected={selected}
-      inCheckSlot={inCheckSlot}
-      targeted={targeted}
-    />
+    <div
+      className="Square"
+      onClick={handleClick}
+      css={{
+        backgroundColor: selected ? SELECTION_GREEN : 'none',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'contain',
+        display: 'flex',
+        justifyContent: 'center',
+        margin: '0px;',
+        outline: getOutline(),
+        outlineOffset: attacked() ? '-2px' : 'none',
+        padding: '0px;',
+      }}
+    >
+      {renderSquareElement()}
+    </div>
   );
 };
 
