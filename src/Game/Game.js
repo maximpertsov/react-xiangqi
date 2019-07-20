c024fa99bb124c89bb01b9cde6f7aae614f39662
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-07-20 16:33:34 -0400

    WIP: game functional component with hooks

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -1,11 +1,11 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core';
 
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import update from 'immutability-helper';
+import { Component, useState, useEffect } from 'react';
 import Board from './Board/Board';
-import { selectMove } from './utils';
+import { selectMove, getNextMoveColor, getNextMovePlayer } from './utils';
 import MoveHistory from './Move/MoveHistory';
 import GameInfo from './GameInfo';
 import LoginForm from '../LoginForm/LoginForm';
@@ -16,6 +16,276 @@ const POLL_INTERVAL = 2500;
 // TODO: define in logic class
 /* eslint-disable-next-line max-len */
 const DEFAULT_FEN = 'rheakaehr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RHEAKAEHR';
+
+const getInitialMoves = (fen = DEFAULT_FEN) => [
+  {
+    piece: undefined,
+    fromPos: undefined,
+    toPos: undefined,
+    board: new XiangqiBoard({ fen }),
+  },
+];
+
+const initialPlayers = [
+  { name: undefined, color: 'red' },
+  { name: undefined, color: 'black' },
+];
+
+const PureGame = ({ gameSlug }) => {
+  const [clientUpdatedAt, setClientUpdatedAt] = useState(null);
+  const [moves, setMoves] = useState(getInitialMoves());
+  const [players, setPlayers] = useState(initialPlayers);
+  const [selectedMoveIdx, setSelectedMoveIdx] = useState(0);
+  const [timer, setTimer] = useState(null);
+  const [username, setUsername] = useState(null);
+
+  // Fetch data utilities
+
+  const fetchMoves = (fen) => {
+    client.getMoves(gameSlug).then((response) => {
+      const { moves: movesData } = response.data;
+      const _moves = getInitialMoves(fen);
+      movesData.reduce(
+        (lastBoard, moveData) => {
+          const { piece, origin: fromPos, destination: toPos } = moveData;
+          const board = lastBoard.move(fromPos, toPos, RefType.RANK_FILE);
+          const move = {
+            piece, fromPos, toPos, board,
+          };
+          _moves.push(move);
+          return move.board;
+        },
+        _moves[0].board,
+      );
+      // TODO: There is one more board than moves.
+      // Watch out for off by 1 errors!
+      setMoves(_moves);
+      setSelectedMoveIdx(_moves.length - 1);
+    });
+  };
+
+  const fetchGame = () => {
+    if (gameSlug === undefined) {
+      setMoves(getInitialMoves());
+      setSelectedMoveIdx(0);
+      return;
+    }
+
+    client.getGame(gameSlug).then((response) => {
+      const { players: _players, initial_fen: fen } = response.data;
+      setPlayers(_players);
+      fetchMoves(fen);
+    });
+  };
+
+  // Polling utilities
+
+  const stopPolling = () => {
+    clearInterval(timer);
+    setTimer(null);
+  };
+
+  // TODO: only poll for move update? Can't do that now because
+  // we don't update active player based on moves
+  const pollForGameUpdate = () => {
+    // TODO: Use current fen instead?
+    const { name: nextMovePlayerName } = getNextMovePlayer(players);
+    if (username === null || username === nextMovePlayerName) {
+      stopPolling();
+      return;
+    }
+
+    if (gameSlug === undefined) return;
+
+    client.getLastUpdate(gameSlug)
+      .then((response) => {
+        const { data: { updated_at: serverUpdatedAt } } = response;
+        if ((clientUpdatedAt === null && serverUpdatedAt !== null)
+          || (clientUpdatedAt < serverUpdatedAt)) {
+          fetchGame();
+          stopPolling();
+          setClientUpdatedAt(serverUpdatedAt);
+        }
+      });
+  };
+
+  const startPolling = () => {
+    setTimer(setInterval(() => pollForGameUpdate(), POLL_INTERVAL));
+  };
+
+  // Lifecycle methods
+
+  useEffect(
+    () => {
+      // will mount
+      fetchGame();
+      startPolling();
+      // will unmount
+      return () => { setTimer(null); };
+    },
+    [gameSlug],
+  );
+
+  // Move updates
+
+  const getPostMovePayload = (board, fromSlot, toSlot) => {
+    const { name: player } = getNextMovePlayer(players, moves);
+    const fromPos = board.getRankFile(fromSlot);
+    const toPos = board.getRankFile(toSlot);
+    const piece = board.getPiece(fromSlot);
+    return {
+      player, piece, fromPos, toPos,
+    };
+  };
+
+  const postMoveToServer = (board, fromSlot, toSlot) => {
+    if (gameSlug === undefined) return;
+
+    const payload = getPostMovePayload(board, fromSlot, toSlot);
+    client
+      .postMove(gameSlug, payload)
+      .then(({ status }) => {
+        if (status !== 201) {
+          fetchGame();
+          startPolling();
+        }
+      })
+      .catch(() => {
+        // TODO: display useful error?
+        fetchGame();
+      });
+  };
+
+  const handleLegalMove = (board, fromSlot, toSlot) => {
+    setMoves((fromMoves) => {
+      const nextMove = {
+        fromPos: board.getRankFile(fromSlot),
+        toPos: board.getRankFile(toSlot),
+        piece: board.getPiece(fromSlot),
+        board: board.move(fromSlot, toSlot),
+      };
+      return update(fromMoves, { $push: [nextMove] });
+    });
+    setSelectedMoveIdx(moves.length);
+
+    postMoveToServer(board, fromSlot, toSlot);
+  };
+
+  //
+  // handleMoveSelect({ idx }) {
+  //   this.setState({ selectedMoveIdx: idx });
+  // }
+  //
+  // getNextMoveColor() {
+  //   const { moves } = this.state;
+  //   if (moves.length === 0) return 'red';
+  //
+  //   // TODO: we don't really need a specific board for this function
+  //   const { piece: lastMovedPiece, board } = moves[moves.length - 1];
+  //
+  //   return board.isRedCode(lastMovedPiece) ? 'black' : 'red';
+  // }
+  //
+  // // TODO: create PlayerManager class?
+  // getNextMovePlayer() {
+  //   const { players } = this.state;
+  //   const nextMoveColor = this.getNextMoveColor();
+  //
+  //   return players.find((p) => p.color === nextMoveColor);
+  // }
+  //
+  // getUserPlayer() {
+  //   const { username, players } = this.state;
+  //   return players.find((p) => p.name === username) || {};
+  // }
+  //
+  // getUserColor() {
+  //   return this.getUserPlayer().color;
+  // }
+  //
+  // setUsername(username) {
+  //   this.setState({ username });
+  //   this.startPolling();
+  // }
+  //
+  // // TODO: add a state that allows players to flip their original orientation
+  // getInitialUserOrientation() {
+  //   if (this.getUserColor() === 'black') return true;
+  //   return false;
+  // }
+  //
+  // getLegalMoves(idx, currentUserOnly = true) {
+  //   const { gameSlug } = this.props;
+  //   const { moves } = this.state;
+  //   const nextMoveColor = this.getNextMoveColor();
+  //   const userColor = this.getUserColor();
+  //   const { board } = selectMove(moves, idx);
+  //   const selectUserMoves = currentUserOnly && gameSlug !== undefined;
+  //
+  //   return board
+  //     .legalMoves()
+  //     .map((toSlots, fromSlot) => {
+  //       if (idx !== -1 && idx !== moves.length - 1) return [];
+  //       if (!board.isColor(nextMoveColor, fromSlot)) return [];
+  //       if (selectUserMoves && !board.isColor(userColor, fromSlot)) return [];
+  //       return toSlots;
+  //     });
+  // }
+
+
+  return (
+    <div
+      className="Game"
+      css={css`
+          display: flex;
+          align-items: center;
+          @media (max-width: 720px) {
+            flex-direction: column;
+          }
+          @media (min-width: 720px) {
+            flex-direction: row;
+          }
+          height: 600px;
+        `}
+    >
+      <Board
+        nextMoveColor={this.getNextMoveColor()}
+        board={selectMove(moves, selectedMoveIdx).board}
+        handleLegalMove={this.handleLegalMove}
+        legalMoves={this.getLegalMoves(selectedMoveIdx)}
+        reversed={this.getInitialUserOrientation()}
+      />
+      <div
+        css={css`
+            justify-content: space-between;
+            flex-direction: column;
+            padding: 0px 50px;
+            height: 100%;
+            max-width: 250px;
+            @media (max-width: 720px) {
+              display: none;
+            }
+            @media (min-width: 720px) {
+              display: flex;
+            }
+          `}
+      >
+        <LoginForm setUsername={this.setUsername} />
+        <GameInfo
+          activePlayer={this.getNextMovePlayer()}
+          userColor={this.getUserColor()}
+          players={players}
+          activeLegalMoves={this.getLegalMoves(-1, false)}
+        />
+        <MoveHistory
+          moves={moves}
+          selectedIdx={selectedMoveIdx}
+          handleMoveSelect={this.handleMoveSelect}
+        />
+      </div>
+    </div>
+  );
+};
 
 class Game extends Component {
   constructor(props) {
