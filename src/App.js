d2ee7718901acc618585fb74a5a2b0e89b005726
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-08-07 09:24:28 -0700

    Get player games

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Game from './Game/Game';
 import 'babel-polyfill';
 import LoginForm from './LoginForm/LoginForm';
+import * as client from './client';
 
 // const GAME_ID = 'ABC123';
 
 const App = () => {
   const [username, setUsername] = useState(undefined);
+  const [games, setGames] = useState([]);
+
+  const fetchGames = useCallback(
+    () => {
+      client.getGameList(username)
+        .then((response) => { setGames(response.data.games); });
+    },
+    [username],
+  );
+
+  useEffect(
+    () => {
+      if (username === undefined) return;
+      fetchGames();
+    },
+    [fetchGames, username],
+  );
 
   const queryParams = new URLSearchParams(window.location.search);
   const gameSlug = queryParams.get('game') || undefined;
+
+  if (gameSlug === undefined) {
+    return (
+      <div>
+        <LoginForm setUsername={setUsername} />
+        {games.map((game) => <div>{game.slug}</div>)}
+      </div>
+    );
+  }
+
   return (
-    <Game
-      gameSlug={gameSlug}
-      username={username}
-      setUsername={setUsername}
-    />
+    <div>
+      <Game
+        gameSlug={gameSlug}
+        username={username}
+        setUsername={setUsername}
+      />
+    </div>
   );
 };
 
