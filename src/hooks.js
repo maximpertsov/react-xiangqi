5e38890d40b70a7860511925b1d0c18bbf93f418
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-10-05 18:06:01 -0400

    Add useEventListener

--- src/hooks.js
+++ src/hooks.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import { useEffect, useReducer, useState } from 'react';
+
+import { useEffect, useRef, useReducer, useState } from 'react';
 
 const dataFetchReducer = (state, action) => {
   switch (action.type) {
@@ -27,7 +28,7 @@ const dataFetchReducer = (state, action) => {
   }
 };
 
-const useDataApi = (initialUrl, initialData) => {
+export const useDataApi = (initialUrl, initialData) => {
   const [url, setUrl] = useState(initialUrl);
 
   const [state, dispatch] = useReducer(dataFetchReducer, {
@@ -65,4 +66,39 @@ const useDataApi = (initialUrl, initialData) => {
   return [state, setUrl];
 };
 
-export default useDataApi;
+// Hook
+export function useEventListener(eventName, handler, element = window) {
+  // Create a ref that stores handler
+  const savedHandler = useRef();
+
+  // Update ref.current value if handler changes.
+  // This allows our effect below to always get latest handler ...
+  // ... without us needing to pass it in effect deps array ...
+  // ... and potentially cause effect to re-run every render.
+  useEffect(() => {
+    savedHandler.current = handler;
+  }, [handler]);
+
+  useEffect(
+    () => {
+      // Make sure element supports addEventListener
+      // On
+      const isSupported = element && element.addEventListener;
+      if (!isSupported) return;
+
+      // Create event listener that calls handler function stored in ref
+      const eventListener = (event) => savedHandler.current(event);
+
+      // Add event listener
+      element.addEventListener(eventName, eventListener);
+
+      // Remove event listener on cleanup
+      return () => {
+        element.removeEventListener(eventName, eventListener);
+      };
+    },
+    [eventName, element], // Re-run if eventName or element changes
+  );
+}
+
+export default {};
