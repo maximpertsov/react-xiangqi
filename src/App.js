c16115e5f0feefdcdb19f9f67e046b4475da095e
maximpertsov <maxim.pertsov@gmail.com>
2019-10-06 02:03:44 -0400

    Use semantic UI for main menu (#38)
    * Let semantic ui manage font
    
    * Dump in semantic UI menu
    
    * Use semantic UI for game menu
    
    * Semantic UI on main menu
    
    * Semantic UI for main menu
    
    * Make App simple again
    
    * Compact segment

--- src/App.js
+++ src/App.js
@@ -1,77 +1,9 @@
-/** @jsx jsx */
-import { jsx, css } from '@emotion/core';
-
-import 'semantic-ui-css/semantic.min.css';
 import 'babel-polyfill';
-import { useCallback, useEffect, useState } from 'react';
-import { MenuButton } from './commonStyles';
-import Game from './Game/Game';
-import GameList from './Game/GameList';
-import LoginForm from './LoginForm/LoginForm';
-import * as client from './client';
+import 'semantic-ui-css/semantic.min.css';
+import React from 'react';
 
-const LOCAL = 'local';
+import MainMenu from './Game/MainMenu';
 
-const App = () => {
-  const [username, setUsername] = useState(undefined);
-  const [gameSlug, setGameSlug] = useState(undefined);
-  const [games, setGames] = useState([]);
-
-  const fetchGames = useCallback(
-    () => {
-      client.getGameList(username)
-        .then((response) => { setGames(response.data.games); });
-    },
-    [username],
-  );
-
-  useEffect(
-    () => {
-      if (username === undefined) return;
-      fetchGames();
-    },
-    [fetchGames, username],
-  );
-
-  switch (gameSlug) {
-    case undefined:
-      return (
-        <div
-          css={css`
-            width:200px;
-          `}
-        >
-          <LoginForm setUsername={setUsername} />
-          <GameList setGameSlug={setGameSlug} games={games} />
-
-          <div
-            css={css`
-              border:1px #CCC solid;
-              margin-top: 15px;
-              margin-bottom: 15px;
-              padding: 5px;
-              width: 100%;
-            `}
-          >
-            Other modes
-            <div>
-              <MenuButton onClick={() => { setGameSlug(LOCAL); }}>
-                Local Play
-              </MenuButton>
-            </div>
-          </div>
-        </div>
-      );
-    case LOCAL:
-      return <Game />;
-    default:
-      return (
-        <Game
-          gameSlug={gameSlug}
-          username={username}
-        />
-      );
-  }
-};
+const App = () => <MainMenu />;
 
 export default App;
