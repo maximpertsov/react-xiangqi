42f0584576078630b05e8031fd15c43a01338ec8
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-11-19 08:14:15 -0500

    Mock API

--- test/e2e/example.test.js
+++ test/e2e/example.test.js
@@ -1,13 +1,25 @@
-import { RequestLogger, Selector } from 'testcafe';
+import { RequestLogger, RequestMock, Selector } from 'testcafe';
 
-const logger = RequestLogger('http://localhost:8000/api/authenticate');
+const APP_URI = 'http://localhost:3000';
+const API_URI = 'http://localhost:8000';
+
+const logger = RequestLogger(`${API_URI}/api/authenticate`);
+const mock = RequestMock()
+  .onRequestTo(`${API_URI}/api/authenticate`)
+  .respond({}, 401, {
+    'access-control-allow-credentials': true,
+    'access-control-allow-origin': APP_URI
+  });
 
 fixture('Getting Started')
-  .page('http://localhost:3000')
+  .page(APP_URI)
   .requestHooks(logger);
 
-test('Failing login', async(t) => {
-  await t
-    .click(Selector('button').withExactText('Login'))
-    .expect(logger.contains((r) => r.response.statusCode === 401)).ok();
-});
+test
+  .requestHooks(mock)(
+    'Failing login', async(t) => {
+      await t
+        .click(Selector('button').withExactText('Login'))
+        .expect(logger.contains((r) => r.response.statusCode === 401)).ok()
+        .expect(Selector('div').withExactText('Login failed').exists).ok();
+    });
