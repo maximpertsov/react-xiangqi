40543acc8b02a9f4c00a8625bca286ffc127b6da
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-11-19 08:24:05 -0500

    Refactor mock

--- test/e2e/example.test.js
+++ test/e2e/example.test.js
@@ -1,25 +1,28 @@
 import { RequestLogger, RequestMock, Selector } from 'testcafe';
 
 const APP_URI = 'http://localhost:3000';
-const API_URI = 'http://localhost:8000';
+const AUTH_URI = 'http://localhost:8000/api/authenticate';
 
-const logger = RequestLogger(`${API_URI}/api/authenticate`);
-const mock = RequestMock()
-  .onRequestTo(`${API_URI}/api/authenticate`)
-  .respond({}, 401, {
-    'access-control-allow-credentials': true,
-    'access-control-allow-origin': APP_URI
-  });
+const ACCESS_CONTROL_HEADERS = {
+  'access-control-allow-credentials': true,
+  'access-control-allow-origin': APP_URI,
+};
+
+const logger = RequestLogger(AUTH_URI);
+const mock = RequestMock().onRequestTo(AUTH_URI);
+
+// Helper functions
+const login = async(t) => t
+  .click(Selector('button').withExactText('Login'));
 
 fixture('Getting Started')
   .page(APP_URI)
   .requestHooks(logger);
 
 test
-  .requestHooks(mock)(
-    'Failing login', async(t) => {
-      await t
-        .click(Selector('button').withExactText('Login'))
-        .expect(logger.contains((r) => r.response.statusCode === 401)).ok()
-        .expect(Selector('div').withExactText('Login failed').exists).ok();
+  .requestHooks(mock.respond({}, 401, ACCESS_CONTROL_HEADERS))(
+    'Failed login', async(t) => {
+      await login(t);
+      await t.expect(logger.contains((r) => r.response.statusCode === 401)).ok();
+      await t.expect(Selector('div').withExactText('Login failed').exists).ok();
     });
