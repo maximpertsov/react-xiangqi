d95c08a97fb5fb109d23c2df64af683c5b9a7f8e
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-11-19 08:46:49 -0500

    WIP: add login success test

--- test/e2e/example.test.js
+++ test/e2e/example.test.js
@@ -9,7 +9,12 @@ const ACCESS_CONTROL_HEADERS = {
 };
 
 const logger = RequestLogger(AUTH_URI);
-const mock = RequestMock().onRequestTo(AUTH_URI);
+const mock_auth_fail = RequestMock()
+  .onRequestTo(AUTH_URI)
+  .respond({}, 401, ACCESS_CONTROL_HEADERS);
+const mock_auth_success = RequestMock()
+  .onRequestTo(AUTH_URI)
+  .respond({}, 201, ACCESS_CONTROL_HEADERS);
 
 // Helper functions
 const login = async(t) => t
@@ -20,9 +25,17 @@ fixture('Getting Started')
   .requestHooks(logger);
 
 test
-  .requestHooks(mock.respond({}, 401, ACCESS_CONTROL_HEADERS))(
-    'Failed login', async(t) => {
+  .requestHooks(mock_auth_fail)(
+    'Fail to login', async(t) => {
       await login(t);
       await t.expect(logger.contains((r) => r.response.statusCode === 401)).ok();
       await t.expect(Selector('div').withExactText('Login failed').exists).ok();
+    });
+
+test
+  .requestHooks(mock_auth_success)(
+    'Login successfully', async(t) => {
+      await login(t);
+      await t.expect(logger.contains((r) => r.response.statusCode === 201)).ok();
+      // await t.expect(Selector('div').withExactText('Games in play').exists).ok();
     });
