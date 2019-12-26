e8bcd7503e2d736c0827d1cb507bdecd602047e9
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-12-26 10:52:42 -0500

    WIP: test login

--- test/e2e/example.test.js
+++ test/e2e/example.test.js
@@ -1,9 +1,15 @@
-import { RequestLogger } from 'testcafe';
+import { RequestLogger, Selector } from 'testcafe';
 
 import { expectLoginFailure, expectUnauthorized, login } from './utils';
 import * as mocks from './mocks';
 
-const logger = RequestLogger(mocks.AUTH_URI); // ([AUTH_URI, GAMES_URI]);
+const logger = RequestLogger(
+  mocks.AUTH_URI,
+  {
+    logResponseBody: true,
+    stringifyResponseBody: true,
+  }
+);
 
 fixture('Login')
   .page(mocks.APP_URI)
@@ -19,13 +25,14 @@ test
       await expectLoginFailure(t);
     });
 
-// // TODO: mock games endpoint
-// // TODO: why isn't access_token in auth response?
-// test
-//   .requestHooks(mockAuthSuccess)(
-//     'Login successfully', async(t) => {
-//       await login(t);
-//       logger.requests.forEach((r) => { console.log(r); });
-//       await t.expect(logger.contains((r) => r.response.statusCode === 201)).ok();
-//       await t.expect(Selector('button').withExactText('Login').exists).notOk();
-//     });
+test
+  .requestHooks(mocks.authFail, mocks.ping)(
+    'Login successfully', async(t) => {
+      await t.expect(Selector('button').withExactText('Login').exists).ok();
+      await t.removeRequestHooks(mocks.authFail)
+      await t.addRequestHooks(mocks.authSuccess)
+      await login(t)
+      // logger.requests.forEach((r) => { console.log(r); });
+      // await t.expect(logger.contains((r) => r.response.statusCode === 201)).ok();
+      // await t.expect(Selector('button').withExactText('Login').exists).notOk();
+    });
