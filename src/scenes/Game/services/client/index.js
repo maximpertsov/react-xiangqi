6c9641e756cca2c87f3ff140850ac94d545dca52
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-12-28 20:36:06 -0500

    eslint update

--- src/scenes/Game/services/client/index.js
+++ src/scenes/Game/services/client/index.js
@@ -4,24 +4,24 @@ import * as selectors from '../../selectors';
 
 const POLL_INTERVAL = 2500;
 
-export const fetchMoves = async(dispatch, {gameSlug}) => {
+export const fetchMoves = async({ dispatch, gameSlug }) => {
   if (gameSlug === undefined) {
     dispatch({ type: 'set_moves', moves: [] });
     return;
   }
 
-  const { data: { moves }} = await client.getMoves(gameSlug);
+  const { data: { moves } } = await client.getMoves(gameSlug);
   dispatch({ type: 'set_moves', moves });
 };
 
-export const fetchGame = async(dispatch, {gameSlug}) => {
+export const fetchGame = async(dispatch, { gameSlug }) => {
   if (gameSlug === undefined) return;
 
-  const { data: { players }} = await client.getGame(gameSlug);
+  const { data: { players } } = await client.getGame(gameSlug);
   dispatch({ type: 'set_players', players });
 };
 
-const canUpdateMoves = ({gameSlug, username, state}) => {
+const canUpdateMoves = ({ gameSlug, username, state }) => {
   if (gameSlug === undefined) return false;
   if (username === undefined) return false;
   if (username === selectors.getNextMovePlayer(state)) return false;
@@ -29,8 +29,8 @@ const canUpdateMoves = ({gameSlug, username, state}) => {
   return true;  
 };
 
-const pollMoves = async(dispatch, {gameSlug, username, state}) => {
-  if (!canUpdateMoves({gameSlug, username, state})) return;
+const pollMoves = async(dispatch, { gameSlug, username, state }) => {
+  if (!canUpdateMoves({ gameSlug, username, state })) return;
 
   const { data: { move_count } } = await client.getMoveCount(gameSlug);
   if (!state.loading && state.moveCount >= move_count) return;
@@ -39,9 +39,9 @@ const pollMoves = async(dispatch, {gameSlug, username, state}) => {
 };
 
 export const setPollMovesInterval = (
-  dispatch, {gameSlug, username, state}
+  dispatch, { gameSlug, username, state }
 ) => setInterval(
-  () => { pollMoves(dispatch, {gameSlug, state, username}); },
+  () => { pollMoves(dispatch, { gameSlug, state, username }); },
   POLL_INTERVAL
 );
 
