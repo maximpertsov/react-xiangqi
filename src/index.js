727411965c2d35a7f68c3d656ed5a0c288d9c499
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-12-30 20:27:59 -0500

    hook up store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import styled from '@emotion/styled';
+
 import App from './App';
+import rootReducer from './reducers';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(rootReducer);
 
 const Wrapper = styled.div`
 body {
@@ -22,7 +27,14 @@ code {
 }
 `;
 
-ReactDOM.render(<Wrapper><App /></Wrapper>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Wrapper>
+      <App />
+    </Wrapper>
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/reducers.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+
+// TODO: move to separate file
+function moves(state = [], action) {
+  switch(action.type) {
+  default:
+    return state;
+  }
+}
+
+export default combineReducers({ moves });
