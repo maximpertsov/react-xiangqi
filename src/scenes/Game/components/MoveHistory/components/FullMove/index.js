bf267d9a6458b337345ab9e6aa98effb13924eb6
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-01-02 09:48:25 -0500

    WIP: full move component

---
+++ src/scenes/Game/components/MoveHistory/components/FullMove/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Move from '../../components/Move';
+
+const FullMove = ({ ordering, redMove, blackMove }) => {
+  /*
+   * display:
+   * - ordering
+   * - red move
+   * - black move if available
+   *
+   * blocked by:
+   * - move must contain id
+   */
+};
+
+// TODO: proptypes
