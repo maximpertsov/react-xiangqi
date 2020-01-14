d1a5b4c9c2035e4397495ab8a4f9d69d5e14097c
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-01-13 20:06:13 -0500

    Remove lint exception

--- src/scenes/Game/reducers/index.js
+++ src/scenes/Game/reducers/index.js
@@ -4,7 +4,6 @@ import moves from 'scenes/Game/reducers/moves';
 import players from 'scenes/Game/reducers/players';
 import selectedMoveId from 'scenes/Game/reducers/selectedMoveId';
 
-/* eslint-disable-next-line complexity */
 const game = combineReducers({ loading, moves, players, selectedMoveId });
 
 export default game;
