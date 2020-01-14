9167ba7608188a44f5647cc6551439a938f4bb63
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-01-13 23:09:45 -0500

    Define move selectors

--- src/scenes/Game/reducers/moves.js
+++ src/scenes/Game/reducers/moves.js
@@ -1,5 +1,9 @@
 import update from 'immutability-helper';
+import sortedIndexBy from 'lodash/sortedIndexBy';
+
 import XiangqiBoard, * as logic from 'services/logic';
+import { Color } from 'services/logic/constants';
+import { isRed } from 'services/logic/utils';
 
 /* eslint-disable-next-line max-len */
 const DEFAULT_FEN = 'rheakaehr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RHEAKAEHR';
@@ -68,3 +72,35 @@ const moves = (state = initialMoves, action) => {
 };
 
 export default moves;
+
+/*******************/
+/***  Selectors  ***/
+/*******************/
+
+const getMoveIndex = (state, moveId) => {
+  const moveIndex = sortedIndexBy(state, { id: moveId }, 'id');
+  if (state[moveIndex].id === moveId) return moveIndex;
+  return -1;
+};
+
+export const getMoveCount = (state) => state.length - 1;
+
+export const getLastMove = (state) => state[getMoveCount(state)];
+
+export const getPreviousMoveId = (state, moveId) => {
+  const moveIndex = getMoveIndex(state, moveId);
+  return moves[Math.min(moveIndex - 1, 0)];
+};
+
+export const getNextMoveId = (state, moveId) => {
+  const moveCount = getMoveCount(state);
+  const moveIndex = getMoveIndex(state, moveId);
+  return moves[Math.max(moveIndex + 1, moveCount)];
+};
+
+export const getNextMoveColor = (state) => {
+  if (getMoveCount(state) === 0) return Color.RED;
+
+  const { piece } = getLastMove(state);
+  return isRed(piece) ? Color.BLACK : Color.RED;
+};
