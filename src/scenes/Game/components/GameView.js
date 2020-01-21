c92992fc348bbbefe22bc160c8957bab413ffd43
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-01-20 20:34:09 -0500

    VERIFY: split out game view

---
+++ src/scenes/Game/components/GameView.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { useSelector } from 'react-redux';
+import { Dimmer, Loader, Segment } from 'semantic-ui-react';
+import { getMoveCount, getOtherPlayer, getCurrentPlayer } from 'reducers';
+import Board from 'components/Board';
+import ConfirmMoveMenu from '../components/ConfirmMoveMenu';
+import GameInfo from '../components/GameInfo';
+import MoveHistory from '../components/MoveHistory';
+import Player from '../components/Player';
+// TODO: smell -- move to reducers
+import * as gameSelectors from '../selectors';
+
+const Wrapper = styled.div`
+align-items: center;
+display: flex;
+flex-direction: column;
+height: 100%;
+justify-content: space-around;
+`;
+
+const GameView = () => {
+  // TODO: this is a massive hack -- should not rely on move count to determine
+  // if this is the initial load
+  const active = useSelector(state =>
+    state.gameSlug !== null
+    && state.loading
+    && getMoveCount(state) <= 1
+  );
+  const currentPlayer = useSelector(state => getCurrentPlayer(state));
+  const hasLegalMoves = useSelector(state =>
+    gameSelectors.hasLegalMoves(state));
+  const legalMoves = useSelector(state => gameSelectors.getLegalMoves(state));
+  const otherPlayer =  useSelector(state => getOtherPlayer(state));
+
+  return (
+    <Dimmer.Dimmable
+      as={Segment}
+      basic
+      blurring
+      dimmed={active}
+    >
+      <Dimmer active={active} page>
+        <Loader>Loading</Loader>
+      </Dimmer>
+      <Wrapper className="Game">
+        <Player {...otherPlayer} />
+        <Board legalMoves={legalMoves} />
+        <Player {...currentPlayer} />
+        <GameInfo hasLegalMoves={hasLegalMoves} />
+        <ConfirmMoveMenu />
+        <MoveHistory />
+      </Wrapper>
+    </Dimmer.Dimmable>
+  );
+};
+
+export default GameView;
--- src/scenes/Game/index.js
+++ src/scenes/Game/index.js
@@ -1,68 +1,29 @@
 import React, { useEffect } from 'react';
-import styled from '@emotion/styled';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
-import { Dimmer, Loader, Segment } from 'semantic-ui-react';
 import useEventListener from '@use-it/event-listener';
 import { makeMove, selectMove } from 'actions';
 import {
-  getMoveCount,
   getLastMove,
-  getSelectedMove,
   getPreviousMove,
   getNextMove,
   getNextMoveColor,
-  getOtherPlayer,
-  getCurrentPlayer,
 } from 'reducers';
-
-import Board from 'components/Board';
-
-import ConfirmMoveMenu from './components/ConfirmMoveMenu';
-import GameInfo from './components/GameInfo';
 import GameClient from './components/GameClient';
-import MoveHistory from './components/MoveHistory';
-import Player from './components/Player';
-
-import * as gameSelectors from './selectors';
-
-const Wrapper = styled.div`
-align-items: center;
-display: flex;
-flex-direction: column;
-height: 100%;
-justify-content: space-around;
-`;
+import GameView from './components/GameView';
 
 const Game = () => {
   const dispatch = useDispatch();
   const autoMove = useSelector(state => state.autoMove);
-  const gameSlug = useSelector(state => state.gameSlug);
-
+  // TODO: make these individual consts
   const selectors = useSelector(
     state => ({
-      // base state fields
-      loading: state.loading,
       // moves
-      moveCount: getMoveCount(state),
       lastMove: getLastMove(state),
-      selectedMove: getSelectedMove(state),
       nextMoveColor: getNextMoveColor(state),
       previousMove: getPreviousMove(state),
       nextMove: getNextMove(state),
-      // players
-      otherPlayer: getOtherPlayer(state),
-      currentPlayer: getCurrentPlayer(state),
-      // game logic
-      legalMoves: gameSelectors.getLegalMoves(state),
-      hasLegalMoves: gameSelectors.hasLegalMoves(state),
-      // other
     }),
     shallowEqual,
-  );
-  // TODO: this is a massive hack -- should not rely on move count to determine
-  // if this is the initial load
-  const active = useSelector(
-    state => gameSlug !== null && state.loading && selectors.moveCount <= 1,
   );
 
   useEffect(
@@ -92,26 +53,10 @@ const Game = () => {
   });
 
   return (
-    <GameClient>
-      <Dimmer.Dimmable
-        as={Segment}
-        basic
-        blurring
-        dimmed={active}
-      >
-        <Dimmer active={active} page>
-          <Loader>Loading</Loader>
-        </Dimmer>
-        <Wrapper className="Game">
-          <Player {...selectors.otherPlayer} />
-          <Board legalMoves={selectors.legalMoves} />
-          <Player {...selectors.currentPlayer} />
-          <GameInfo hasLegalMoves={selectors.hasLegalMoves} />
-          <ConfirmMoveMenu />
-          <MoveHistory />
-        </Wrapper>
-      </Dimmer.Dimmable>
-    </GameClient>
+    <div>
+      <GameClient />
+      <GameView />
+    </div>
   );
 };
 
