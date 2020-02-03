906c67832a692c97ef3f9fa0149c3ded9aa1c30f
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-02-03 06:47:17 -0500

    Mount the entire app
    Tests can pass if `mount` is replaced with `shallow`. It might be
    preferable to do this and test individual components.

--- test/App.test.js
+++ test/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import configureMockStore from 'redux-mock-store';
 import App from '../src/App';
 
+const mockStore = configureMockStore();
+const store = mockStore({});
+
+Enzyme.configure({ adapter: new Adapter() });
+
 test('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  mount(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
 });
