7cac6a52d38a5ff38b97bcd331a5e6de1875822d
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-02-03 06:58:55 -0500

    Test works again

--- test/App.test.js
+++ test/App.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import configureMockStore from 'redux-mock-store';
 import App from '../src/App';
 
-const mockStore = configureMockStore();
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 const store = mockStore({
   loginForm: {},
   games: [],
