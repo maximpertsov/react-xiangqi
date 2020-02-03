819e0cf1a53be2f3e58638ee5fccc5364b85022a
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-02-03 07:41:06 -0500

    Render whole app and check snapshot

--- test/App.test.js
+++ test/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import Enzyme, { mount } from 'enzyme';
+import Enzyme, { render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import configureMockStore from 'redux-mock-store';
 import App from '../src/App';
@@ -16,9 +16,10 @@ const store = mockStore({
 Enzyme.configure({ adapter: new Adapter() });
 
 test('renders without crashing', () => {
-  mount(
+  const wrapper = render(
     <Provider store={store}>
       <App />
     </Provider>,
   );
+  expect(wrapper).toMatchSnapshot();
 });
