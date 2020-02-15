234744f6c22d19d3c465815a13a01995f4cc22ca
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-02-15 14:23:03 -0500

    WIP: new fen

--- src/services/logic/fen.js
+++ src/services/logic/fen.js
@@ -1,3 +1,22 @@
-export const decode = fen => fen;
+import zipObject from 'lodash/zipObject';
+import update from 'lodash/update';
+
+const FEN_FIELDS = [
+  'placement',
+  'activeColor',
+  'castling',
+  'enPassant',
+  'halfmoves',
+  'fullmoves',
+];
+
+export const decodeFields = fen => {
+  const result = zipObject(FEN_FIELDS, fen.split(' '));
+  update(result, 'halfmoves', parseInt);
+  update(result, 'fullmoves', parseInt);
+  return result;
+};
+
+export const decode = fen => decodeFields(fen);
 
 export default {};
--- test/logic/fen.test.js
+++ test/logic/fen.test.js
@@ -4,5 +4,25 @@ test('convert fen to board', () => {
   const fen =
     'rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR w - - 0 1';
 
-  expect(decode(fen)).toBe(undefined);
+  const expectedPlacement = [
+    ['r', 'n', 'b', 'a', 'k', 'a', 'b', 'n', 'r'],
+    Array(9).fill(null),
+    [null, 'c', null, null, null, null, null, 'c', null],
+    ['p', null, 'p', null, 'p', null, 'p', null, 'p'],
+    Array(9).fill(null),
+    Array(9).fill(null),
+    ['P', null, 'P', null, 'P', null, 'P', null, 'P'],
+    [null, 'C', null, null, null, null, null, 'C', null],
+    Array(9).fill(null),
+    ['R', 'N', 'B', 'A', 'K', 'A', 'B', 'N', 'R'],
+  ].flat();
+
+  expect(decode(fen)).toMatchObject({
+    placement: expectedPlacement,
+    activeColor: 'w',
+    castling: '-',
+    enPassant: '-',
+    halfmoves: 0,
+    fullmoves: 0,
+  });
 });
