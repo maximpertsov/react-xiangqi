e8e5c109e82f7da3affadcfc53819c23228b3128
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-02-15 16:21:52 -0500

    Encode square

--- src/services/logic/square.js
+++ src/services/logic/square.js
@@ -1,4 +1,12 @@
-export const encode = slot => slot;
+import { getRankFile } from './utils';
+
+const RANKS = ['10', '9', '8', '7', '6', '5', '4', '3', '2', '1'];
+const FILES = 'ABCDEFGHI'.split('');
+
+export const encode = slot => {
+  const [rank, file] = getRankFile(slot);
+  return `${FILES[file]}${RANKS[rank]}`;
+};
 
 export const decode = square => square;
 
