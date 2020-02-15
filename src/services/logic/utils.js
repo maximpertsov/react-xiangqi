e26fdad92450d54f0fb480c6db36d0664061aa24
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-02-15 13:30:39 -0500

    prettier

--- src/services/logic/utils.js
+++ src/services/logic/utils.js
@@ -11,48 +11,38 @@ import {
 } from './constants';
 
 // Pieces
-export const isPawn = (piece) => (
-  piece === Piece.Black.PAWN || piece === Piece.Red.PAWN
-);
-export const isChariot = (piece) => (
-  piece === Piece.Black.CHARIOT || piece === Piece.Red.CHARIOT
-);
-export const isHorse = (piece) => (
-  piece === Piece.Black.HORSE || piece === Piece.Red.HORSE
-);
-export const isElephant = (piece) => (
-  piece === Piece.Black.ELEPHANT || piece === Piece.Red.ELEPHANT
-);
-export const isGeneral = (piece) => (
-  piece === Piece.Black.GENERAL || piece === Piece.Red.GENERAL
-);
-export const isCannon = (piece) => (
-  piece === Piece.Black.CANNON || piece === Piece.Red.CANNON
-);
-export const isAdvisor = (piece) => (
-  piece === Piece.Black.ADVISOR || piece === Piece.Red.ADVISOR
-);
+export const isPawn = piece =>
+  piece === Piece.Black.PAWN || piece === Piece.Red.PAWN;
+export const isChariot = piece =>
+  piece === Piece.Black.CHARIOT || piece === Piece.Red.CHARIOT;
+export const isHorse = piece =>
+  piece === Piece.Black.HORSE || piece === Piece.Red.HORSE;
+export const isElephant = piece =>
+  piece === Piece.Black.ELEPHANT || piece === Piece.Red.ELEPHANT;
+export const isGeneral = piece =>
+  piece === Piece.Black.GENERAL || piece === Piece.Red.GENERAL;
+export const isCannon = piece =>
+  piece === Piece.Black.CANNON || piece === Piece.Red.CANNON;
+export const isAdvisor = piece =>
+  piece === Piece.Black.ADVISOR || piece === Piece.Red.ADVISOR;
 
 // Board square locators
-export const getSlot = (rank, file) => file + (rank * FILE_COUNT);
-export const getRank = (slot) => Math.floor(slot / FILE_COUNT);
-export const getFile = (slot) => slot % FILE_COUNT;
-export const getRankFile = (slot) => [getRank(slot), getFile(slot)];
+export const getSlot = (rank, file) => file + rank * FILE_COUNT;
+export const getRank = slot => Math.floor(slot / FILE_COUNT);
+export const getFile = slot => slot % FILE_COUNT;
+export const getRankFile = slot => [getRank(slot), getFile(slot)];
 
 // Colors
-export const isBlack = (piece) => BLACK_PIECES.includes(piece);
-export const isRed = (piece) => RED_PIECES.includes(piece);
-export const sameColor = (piece1, piece2) => (
-  (isRed(piece1) && isRed(piece2)) || (isBlack(piece1) && isBlack(piece2))
-);
+export const isBlack = piece => BLACK_PIECES.includes(piece);
+export const isRed = piece => RED_PIECES.includes(piece);
+export const sameColor = (piece1, piece2) =>
+  (isRed(piece1) && isRed(piece2)) || (isBlack(piece1) && isBlack(piece2));
 
 // Palace
-export const inBlackPalace = (slot) => BLACK_PALACE
-  .map((pos) => getSlot(...pos))
-  .includes(slot);
-export const inRedPalace = (slot) => RED_PALACE
-  .map((pos) => getSlot(...pos))
-  .includes(slot);
+export const inBlackPalace = slot =>
+  BLACK_PALACE.map(pos => getSlot(...pos)).includes(slot);
+export const inRedPalace = slot =>
+  RED_PALACE.map(pos => getSlot(...pos)).includes(slot);
 
 // Move
 export const tryMove = (slot, rankMove, fileMove) => {
@@ -79,8 +69,8 @@ export const tryMarch = (
   return restSlots;
 };
 
-export const tryMarchMoves = (slot, moves, steps) => moves
-  .reduce(
+export const tryMarchMoves = (slot, moves, steps) =>
+  moves.reduce(
     (acc, move) => acc.concat(tryMarch(slot, move[0], move[1], steps)),
     [],
   );
@@ -96,13 +86,12 @@ export const diagonalSlots = (slot, radius) => {
 };
 
 // FEN
-export const fromFenRow = (row) => row.split('').reduce((acc, ch) => {
-  const val = +ch;
-  const newItems = Number.isNaN(val) ? [ch] : Array(val).fill(null);
-  return acc.concat(newItems);
-}, []);
+export const fromFenRow = row =>
+  row.split('').reduce((acc, ch) => {
+    const val = +ch;
+    const newItems = Number.isNaN(val) ? [ch] : Array(val).fill(null);
+    return acc.concat(newItems);
+  }, []);
 
-export const fromFen = (fen) => fen.split('/').reduce(
-  (acc, row) => acc.concat(fromFenRow(row)),
-  [],
-);
+export const fromFen = fen =>
+  fen.split('/').reduce((acc, row) => acc.concat(fromFenRow(row)), []);
