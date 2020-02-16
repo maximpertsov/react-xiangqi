24119b8b9e51f907824229aefad70e618dc55ff5
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-02-16 12:07:39 -0500

    Define new move operation

---
+++ src/services/logic/move.js
@@ -0,0 +1,3 @@
+export const makeMove = (placement, move) => placement;
+
+export default {};
---
+++ test/logic/move.test.js
@@ -0,0 +1,23 @@
+import flatten from 'lodash/flatten';
+import { decode } from 'services/logic/fen';
+import { makeMove } from 'services/logic/move';
+
+test('update board placement by move', () => {
+  const fen =
+    'rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR w - - 0 1';
+  const initialPlacement = decode(fen).placement;
+  const expectedPlacement = flatten([
+    [null, 'n', 'b', 'a', 'k', 'a', 'b', 'n', 'r'],
+    ['r', ...Array(8).fill(null)],
+    [null, 'c', null, null, null, null, null, 'c', null],
+    ['p', null, 'p', null, 'p', null, 'p', null, 'p'],
+    Array(9).fill(null),
+    Array(9).fill(null),
+    ['P', null, 'P', null, 'P', null, 'P', null, 'P'],
+    [null, 'C', null, null, null, null, null, 'C', null],
+    Array(9).fill(null),
+    ['R', 'N', 'B', 'A', 'K', 'A', 'B', 'N', 'R'],
+  ]);
+
+  expect(makeMove(initialPlacement, 'A10A9')).toStrictEqual(expectedPlacement);
+});
