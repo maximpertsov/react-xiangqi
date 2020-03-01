68d4f91fb5fa3c806273315d74a32da4470beb48
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-03-01 18:53:19 -0500

    WIP: moves test

--- test/reducers/moves.test.js
+++ test/reducers/moves.test.js
@@ -1,4 +1,14 @@
 describe('move reducers', () => {
-  // TODO: add a test
-  // test('', () => {});
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual([
+      {
+        id: 0,
+        fromSlot: undefined,
+        toSlot: undefined,
+        piece: undefined,
+        board: new XiangqiBoard({ fen: DEFAULT_FEN }),
+        pending: false,
+      },
+    ]);
+  });
 });
