1ac1ba4fab39c46c95d467a6b44aa3f1832641dd
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-03-01 22:25:12 -0500

    tdd add move

--- test/reducers/moves.test.js
+++ test/reducers/moves.test.js
@@ -2,16 +2,22 @@ import moves, { DEFAULT_FEN } from 'reducers/moves';
 import XiangqiBoard from 'services/logic';
 
 describe('move reducers', () => {
+  const initialMove = {
+    id: 0,
+    fromSlot: undefined,
+    toSlot: undefined,
+    piece: undefined,
+    board: new XiangqiBoard({ fen: DEFAULT_FEN }),
+    pending: false,
+  };
+
   it('should return the initial state', () => {
-    expect(moves(undefined, {})).toEqual([
-      {
-        id: 0,
-        fromSlot: undefined,
-        toSlot: undefined,
-        piece: undefined,
-        board: new XiangqiBoard({ fen: DEFAULT_FEN }),
-        pending: false,
-      },
-    ]);
+    expect(moves(undefined, {})).toEqual([initialMove]);
+  });
+
+  it('should add a move', () => {
+    const action = { type: 'add_move' };
+
+    expect(moves(undefined, action)).toEqual([initialMove]);
   });
 });
