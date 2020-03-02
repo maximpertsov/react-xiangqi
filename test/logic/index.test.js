06bf6671d5dc26033653cae66a07896673c5d572
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-03-02 10:51:05 -0500

    Test old functionality

---
+++ test/logic/index.test.js
@@ -0,0 +1,24 @@
+import XiangiBoard from 'services/logic';
+
+describe('board', () => {
+  const fen =
+    'rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR w - - 0 1';
+
+  test('move', () => {
+    const board = new XiangiBoard({ fen });
+
+    expect(board.placement[0]).toBe('r');
+    expect(board.placement[9]).toBe(null);
+
+    const newBoard = board.move(0, 9);
+    // const newBoard = board.move('a10a9');
+
+    // Initial board did not change
+    expect(board.placement[0]).toBe('r');
+    expect(board.placement[9]).toBe(null);
+
+    // Rook has moved forward on new board
+    expect(newBoard.placement[0]).toBe(null);
+    expect(newBoard.placement[9]).toBe('r');
+  });
+});
