2c885b98a97ff1a75e473c9695b120277815a322
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-03-08 13:22:36 -0400

    WIP: remove unused utils

--- src/services/logic/utils.js
+++ src/services/logic/utils.js
@@ -1,13 +1,8 @@
 import {
   Piece,
-  RANK_COUNT,
   FILE_COUNT,
   BLACK_PIECES,
   RED_PIECES,
-  BLACK_PALACE,
-  RED_PALACE,
-  ORTHOGONAL_MOVES,
-  DIAGONAL_MOVES,
 } from './constants';
 
 // Pieces
@@ -37,50 +32,3 @@ export const isBlack = piece => BLACK_PIECES.includes(piece);
 export const isRed = piece => RED_PIECES.includes(piece);
 export const sameColor = (piece1, piece2) =>
   (isRed(piece1) && isRed(piece2)) || (isBlack(piece1) && isBlack(piece2));
-
-// Palace
-export const inBlackPalace = slot =>
-  BLACK_PALACE.map(pos => getSlot(...pos)).includes(slot);
-export const inRedPalace = slot =>
-  RED_PALACE.map(pos => getSlot(...pos)).includes(slot);
-
-// Move
-export const tryMove = (slot, rankMove, fileMove) => {
-  const [rank, file] = getRankFile(slot);
-  const newRank = rankMove + rank;
-  const newFile = fileMove + file;
-  if (newRank < 0 || newRank >= RANK_COUNT) return null;
-  if (newFile < 0 || newFile >= FILE_COUNT) return null;
-  return getSlot(newRank, newFile);
-};
-
-// TODO: make this non-recursive?
-export const tryMarch = (
-  slot,
-  rankMove,
-  fileMove,
-  steps = Math.max(RANK_COUNT, FILE_COUNT),
-) => {
-  if (steps < 1) return [];
-  const nextSlot = tryMove(slot, rankMove, fileMove);
-  if (nextSlot === null) return [];
-  const restSlots = tryMarch(nextSlot, rankMove, fileMove, steps - 1);
-  restSlots.push(nextSlot);
-  return restSlots;
-};
-
-export const tryMarchMoves = (slot, moves, steps) =>
-  moves.reduce(
-    (acc, move) => acc.concat(tryMarch(slot, move[0], move[1], steps)),
-    [],
-  );
-
-export const orthogonalSlots = (slot, radius) => {
-  const steps = radius === undefined ? 1 : radius;
-  return tryMarchMoves(slot, ORTHOGONAL_MOVES, steps);
-};
-
-export const diagonalSlots = (slot, radius) => {
-  const steps = radius === undefined ? 1 : radius;
-  return tryMarchMoves(slot, DIAGONAL_MOVES, steps);
-};
