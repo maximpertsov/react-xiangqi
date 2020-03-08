19618ca341d93b4108362ce2d83e7bbff3a90c34
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-03-08 13:30:42 -0400

    remove is<piece> functions

--- src/services/logic/utils.js
+++ src/services/logic/utils.js
@@ -1,25 +1,4 @@
-import {
-  Piece,
-  FILE_COUNT,
-  BLACK_PIECES,
-  RED_PIECES,
-} from './constants';
-
-// Pieces
-export const isPawn = piece =>
-  piece === Piece.Black.PAWN || piece === Piece.Red.PAWN;
-export const isChariot = piece =>
-  piece === Piece.Black.CHARIOT || piece === Piece.Red.CHARIOT;
-export const isHorse = piece =>
-  piece === Piece.Black.HORSE || piece === Piece.Red.HORSE;
-export const isElephant = piece =>
-  piece === Piece.Black.ELEPHANT || piece === Piece.Red.ELEPHANT;
-export const isGeneral = piece =>
-  piece === Piece.Black.GENERAL || piece === Piece.Red.GENERAL;
-export const isCannon = piece =>
-  piece === Piece.Black.CANNON || piece === Piece.Red.CANNON;
-export const isAdvisor = piece =>
-  piece === Piece.Black.ADVISOR || piece === Piece.Red.ADVISOR;
+import { FILE_COUNT, BLACK_PIECES, RED_PIECES } from './constants';
 
 // Board square locators
 export const getSlot = (rank, file) => file + rank * FILE_COUNT;
