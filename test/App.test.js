7fefd3b7dfb69b3dae5cffc591161a7fb61aed54
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-04-05 17:56:28 -0400

    use global test setup

--- test/App.test.js
+++ test/App.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import Enzyme, { render } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { render } from 'enzyme';
 import configureMockStore from 'redux-mock-store';
 import App from '../src/App';
 
@@ -12,8 +11,6 @@ const store = mockStore({
   loginForm: {},
   games: [],
 });
-
-Enzyme.configure({ adapter: new Adapter() });
 
 test('renders without crashing', () => {
   const wrapper = render(
--- test/actions/fetchGame.test.js
+++ test/actions/fetchGame.test.js
@@ -1,15 +1,9 @@
-import 'regenerator-runtime/runtime';
 import thunk from 'redux-thunk';
 import configureMockStore from 'redux-mock-store';
 import fetchGame from 'actions/fetchGame';
 
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
-
-Enzyme.configure({ adapter: new Adapter() });
 
 describe('fetch game', () => {
   const store = mockStore({});
