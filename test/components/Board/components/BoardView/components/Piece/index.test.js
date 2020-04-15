19096af42a820769db9b3a28b7171221b30c8ca1
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-04-14 22:57:06 -0400

    Shallow renders

--- test/components/Board/components/BoardView/components/Piece/index.test.js
+++ test/components/Board/components/BoardView/components/Piece/index.test.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { shallow } from 'enzyme';
 import Piece from 'components/Board/components/BoardView/components/Piece';
 import { ALL_PIECES } from 'services/logic/constants';
 
 describe('Piece', () => {
   test.each(ALL_PIECES)('with code %s', code => {
-    const wrapper = render(<Piece code={code} />);
+    const wrapper = shallow(<Piece code={code} />);
     expect(wrapper).toMatchSnapshot();
   });
 
   test.each(ALL_PIECES)('moving with code %s', code => {
-    const wrapper = render(<Piece code={code} moveX={1} moveY={2} />);
+    const wrapper = shallow(<Piece code={code} moveX={1} moveY={2} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
