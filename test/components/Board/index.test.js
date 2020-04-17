f4bab3878aeb50b6002e1688efcd44f2605ab7f8
maximpertsov <maxim.pertsov@gmail.com>
2020-04-16 23:31:56 -0400

    Test Board component (#80)
    * remove unnecessary index.js
    
    * Test all pieces
    
    * absolute import
    
    * Add basic board test
    
    * Square test
    
    * Install jest-enzyme
    
    * Fix enzyme jest
    
    * Shallow renders
    
    * shallow square
    
    * Mount boardview
    
    * WIP: find square by key
    
    * clear slots on board
    
    * WIP: click
    
    * WIP: logging
    
    * Install redux-actions-assertions
    
    * WIP: redux-actions-assertions
    
    * Revert "WIP: redux-actions-assertions"
    
    This reverts commit 0c471456cb057a66f377277c35377f7e33784226.
    
    * Select a square
    
    * Remove board view test
    
    * Remove react assertions test
    
    * Remove testcafe
    
    * Select and deselect
    
    * Refactor board test
    
    * board snapshot
    
    * Remove Square and Piece tests

---
+++ test/components/Board/index.test.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { mount, render } from 'enzyme';
+
+import { applyMiddleware, createStore, compose } from 'redux';
+import rootReducer from 'reducers';
+
+import Board from 'components/Board';
+
+import initialPlacementOnly from './fixtures/initialPlacementOnly.json';
+
+const initialState = {
+  gameSlug: null,
+  showGame: true,
+  moves: initialPlacementOnly,
+  players: [
+    {
+      color: 'red',
+    },
+    {
+      color: 'black',
+    },
+  ],
+  selectedMoveId: null,
+  animationOffset: [0, 0],
+  selectedSquare: null,
+};
+
+const getBoard = (overrides = {}) => {
+  const store = compose(applyMiddleware(thunk))(createStore)(rootReducer, {
+    ...initialState,
+    ...overrides,
+  });
+  return (
+    <Provider store={store}>
+      <Board />
+    </Provider>
+  );
+};
+
+describe('Board', () => {
+  test('renders without crashing', () => {
+    expect(render(getBoard())).toMatchSnapshot();
+  });
+
+  test('select and deselect a square', () => {
+    const wrapper = mount(getBoard());
+    expect(wrapper.exists('SelectionIndicator')).toBe(false);
+
+    const e4 = wrapper.find('Square').findWhere(node => node.key() == 'e4');
+    e4.children().simulate('click');
+    wrapper.update();
+    expect(wrapper.exists('SelectionIndicator')).toBe(true);
+
+    e4.children().simulate('click');
+    wrapper.update();
+    expect(wrapper.exists('SelectionIndicator')).toBe(false);
+
+    wrapper.unmount();
+  });
+});
