30b99abe3127c17e54160c98a5a989224ec7bf00
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-04-18 18:46:59 -0400

    Get rid of useLayoutEffectWarning
    https://stackoverflow.com/questions/58070996/how-to-fix-the-warning-uselayouteffect-does-nothing-on-the-server

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,4 @@
+import React from "react" 
 import 'regenerator-runtime/runtime';
 
 import Enzyme from 'enzyme';
@@ -6,3 +7,6 @@ import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({
   adapter: new Adapter(),
 });
+
+// https://stackoverflow.com/questions/58070996/how-to-fix-the-warning-uselayouteffect-does-nothing-on-the-server
+React.useLayoutEffect = React.useEffect 
