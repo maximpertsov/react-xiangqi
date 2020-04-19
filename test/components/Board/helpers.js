95fdf44af21e13f7378bfb72306524600f783d60
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-04-19 12:27:29 -0400

    Better selection tests

--- test/components/Board/helpers.js
+++ test/components/Board/helpers.js
@@ -1,8 +1,7 @@
-const getSquareNode = (wrapper, square) =>
-  wrapper.find('Square').findWhere(node => node.key() == square);
+const getSquareNodes = (wrapper, squares) =>
+  wrapper.find('Square').findWhere(node => squares.includes(node.key()));
 
-const getSquareNodesWrapper = (wrapper, squares) =>
-  wrapper.find('Square').filterWhere(node => squares.includes(node.key()));
+const getSquareNode = (wrapper, square) => getSquareNodes(wrapper, [square]);
 
 export const clickSquare = (wrapper, square) => {
   getSquareNode(wrapper, square)
@@ -15,17 +14,13 @@ export const clickSquare = (wrapper, square) => {
 };
 
 export const expectSquaresToBeSelected = (wrapper, squares) => {
-  const selectedSquares = getSquareNodesWrapper(wrapper, squares).find(
-    'SelectionIndicator',
-  );
-  expect(selectedSquares).toHaveLength(squares.length);
+  const nodes = getSquareNodes(wrapper, squares).find('SelectionIndicator');
+  expect(nodes).toHaveLength(squares.length);
 };
 
 export const expectSquaresToBeTargeted = (wrapper, squares) => {
-  const targetedSquares = getSquareNodesWrapper(wrapper, squares).find(
-    'TargetIndicator',
-  );
-  expect(targetedSquares).toHaveLength(squares.length);
+  const nodes = getSquareNodes(wrapper, squares).find('TargetIndicator');
+  expect(nodes).toHaveLength(squares.length);
 };
 
 export const expectToHavePiece = (wrapper, square, piece) => {
