be30523da7ad7ad39d473c72ec4a22b4bd16322f
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-04-25 14:12:44 -0400

    Spike new confirm menu test

---
+++ src/scenes/Game/components/ConfirmPositionMenu.js
@@ -0,0 +1,37 @@
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+// import { cancelMoves, confirmMoves, postMove } from 'actions';
+
+import ConfirmMenu from 'components/ConfirmMenu';
+
+const ConfirmPositionMenu = () => {
+  const dispatch = useDispatch();
+
+  const gameSlug = useSelector(state => state.gameSlug);
+  const pendingPositionId = useSelector(state => state.pendingPositionId);
+  // const username = useSelector(state => state.username);
+
+  const confirmPosition = useCallback(async () => {
+    // TODO: Not implemented yet
+    dispatch({ type: 'update_pending_position', pendingPositionId: null });
+
+    // TODO: Post to server
+  }, [dispatch]);
+
+  const cancelPosition = useCallback(() => {
+    dispatch({ type: 'remove_position', id: pendingPositionId });
+  }, [dispatch, pendingPositionId]);
+
+  return (
+    <ConfirmMenu
+      label="Confirm move?"
+      yesHandler={confirmPosition}
+      noHandler={cancelPosition}
+      show={pendingPositionId !== null}
+      disabled={gameSlug === null}
+    />
+  );
+};
+
+export default ConfirmPositionMenu;
---
+++ test/scenes/Game/components/ConfirmPositionMenu.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { mount, render } from 'enzyme';
+
+import { applyMiddleware, createStore, compose } from 'redux';
+import rootReducer from 'reducers';
+
+import ConfirmPositionMenu from 'scenes/Game/components/ConfirmPositionMenu';
+
+const initialState = {
+  gameSlug: null,
+  showGame: true,
+  positions: [{ id: 0 }, { id: 1 }],
+  pendingPositionId: null,
+  players: [
+    {
+      color: 'red',
+    },
+    {
+      color: 'black',
+    },
+  ],
+  selectedMoveId: null,
+  animationOffset: [0, 0],
+  selectedSquare: null,
+};
+
+const getStore = (overrides = {}) =>
+  compose(applyMiddleware(thunk))(createStore)(rootReducer, {
+    ...initialState,
+    ...overrides,
+  });
+
+const getComponent = store => (
+  <Provider store={store}>
+    <ConfirmPositionMenu />
+  </Provider>
+);
+
+describe('ConfirmPositionMenu', () => {
+  test('renders without crashing', () => {
+    const wrapper = render(getComponent(getStore()));
+    expect(wrapper).toMatchSnapshot();
+  });
+});
