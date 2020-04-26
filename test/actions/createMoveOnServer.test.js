161a73aa14c37b6980f0df50c5e2c4c4162adb4d
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-04-26 00:47:06 -0400

    WIP: fill out server move tests

--- test/actions/createMoveOnServer.test.js
+++ test/actions/createMoveOnServer.test.js
@@ -1,16 +1,56 @@
-import createMoveOnServer from 'services/client';
+import createMoveOnServer from 'actions/createMoveOnServer';
+import * as client from 'services/client';
 
 describe('create move on server', () => {
   const position = { id: 1, move: 'a1a2' };
   const username = 'user';
 
-  test('no request if no game slug', () => {
-    createMoveOnServer({ gameSlug: null, position, username });
+  describe('not a persisted game', () => {
+    const gameSlug = null;
+
+    test('action does not make an API request', async () => {
+      const spy = jest.spyOn(client, 'postMove');
+
+      await createMoveOnServer({ gameSlug, position, username })();
+      expect(spy).toHaveBeenCalledTimes(0);
+
+      spy.mockRestore();
+    });
   });
-  test('successful request', () => {
-    createMoveOnServer({ gameSlug: 'ABC123', position, username });
-  });
-  test('failed request', () => {
-    createMoveOnServer({ gameSlug: 'ABC123', position, username });
+
+  describe('a persisted game', () => {
+    const gameSlug = 'ABC123';
+
+    test('successful request', async () => {
+      const spy = jest
+        .spyOn(client, 'postMove')
+        .mockImplementation(() => Promise.resolve({ status: 200 }));
+
+      await createMoveOnServer({ gameSlug, position, username })();
+
+      expect(spy).toHaveBeenCalledWith({
+        gameSlug,
+        move: position.move,
+        username,
+      });
+
+      spy.mockRestore();
+    });
+
+    test('failed request', async () => {
+      const spy = jest
+        .spyOn(client, 'postMove')
+        .mockImplementation(() => Promise.resolve({ status: 400 }));
+
+      await createMoveOnServer({ gameSlug, position, username })();
+
+      expect(spy).toHaveBeenCalledWith({
+        gameSlug,
+        move: position.move,
+        username,
+      });
+
+      spy.mockRestore();
+    });
   });
 });
