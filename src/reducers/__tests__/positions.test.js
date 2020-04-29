2e5e4a2c9c567332d62f527956716d22d33e54b8
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-04-28 23:59:29 -0400

    clean up moves tests

--- src/reducers/__tests__/positions.test.js
+++ src/reducers/__tests__/positions.test.js
@@ -1,46 +1,52 @@
 import positions, { getLastMove, getMoveById } from 'reducers/positions';
 
+const createMove = (options = {}) => ({
+  id: undefined,
+  fen: undefined,
+  givesCheck: undefined,
+  legalMoves: undefined,
+  move: undefined,
+  ...options,
+});
+
 describe('positions reducer', () => {
   it('should return the default state', () => {
     expect(positions(undefined, {})).toEqual([]);
   });
 
   describe('add position', () => {
-    const action = { type: 'GAME/POSITIONS/ADD', payload: {}  };
+    const action = { type: 'GAME/POSITIONS/ADD', payload: {} };
 
     it('should add the first position with id = 0', () => {
       const currentState = [];
-      const expectedNewState = [
-        {
-          id: 0,
-          fen: undefined,
-          givesCheck: undefined,
-          legalMoves: undefined,
-          move: undefined,
-          pending: undefined,
-        },
-      ];
+      const expectedNewState = [createMove({ id: 0 })];
       expect(positions(currentState, action)).toEqual(expectedNewState);
     });
 
     it('should add a position with an incremented id', () => {
-      const currentState = [{ id: 0 }];
-      const expectedNewState = [
-        { id: 0 },
-        {
-          id: 1,
-          fen: undefined,
-          givesCheck: undefined,
-          legalMoves: undefined,
-          move: undefined,
-        },
-      ];
+      const currentState = [createMove({ id: 0 })];
+      const expectedNewState = [createMove({ id: 0 }), createMove({ id: 1 })];
       expect(positions(currentState, action)).toStrictEqual(expectedNewState);
     });
   });
 
   it('should remove given position', () => {
     const action = { type: 'remove_position', id: 1 };
+
+    const currentState = [
+      createMove({ id: 0, move: null }),
+      createMove({ id: 1, move: 'a1a2' }),
+      createMove({ id: 2, move: 'a10a9' }),
+    ];
+    const expectedNewState = [
+      createMove({ id: 0, move: null }),
+      createMove({ id: 2, move: 'a10a9' }),
+    ];
+    expect(positions(currentState, action)).toEqual(expectedNewState);
+  });
+
+  it.skip('should update the given position', () => {
+    const action = { type: 'GAME/POSITIONS/UPDATE', id: 1 };
 
     const currentState = [
       { id: 0, move: null },
