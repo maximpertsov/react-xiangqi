9fb7f2bf4c1f2e759c458f95288a9e948cb2e425
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-05-02 13:04:27 -0400

    highlight drop zone

--- src/components/Board/components/BoardView/components/Square.js
+++ src/components/Board/components/BoardView/components/Square.js
@@ -2,7 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 
+import { useDrop } from 'react-dnd';
+
 const Wrapper = styled.div`
+  background-color: ${props =>
+    props.isOver ? 'rgba(255, 255, 153, 0.7)' : 'none'};
+  border-radius: 50%;
   display: flex;
   justify-content: center;
   margin: 0px;
@@ -10,11 +15,28 @@ const Wrapper = styled.div`
   position: relative;
 `;
 
-const Square = ({ children, handleClick }) => (
-  <Wrapper className="Square" onClick={handleClick}>
-    {children}
-  </Wrapper>
-);
+const Square = ({ children, handleClick }) => {
+  const [{ isOver }, drop] = useDrop({
+    accept: 'PIECE',
+    drop: () => true,
+    canDrop: () => true,
+    collect: monitor => ({
+      isOver: !!monitor.isOver(),
+      canDrop: !!monitor.canDrop(),
+    }),
+  });
+
+  return (
+    <Wrapper
+      className="Square"
+      ref={drop}
+      onClick={handleClick}
+      isOver={isOver}
+    >
+      {children}
+    </Wrapper>
+  );
+};
 
 Square.propTypes = {
   children: PropTypes.node.isRequired,
