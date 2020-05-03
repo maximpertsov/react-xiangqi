f87cf08c93e5335e740698784b04bd8cc6fe2602
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-05-03 10:01:43 -0400

    use reselect in square

--- src/components/Board/components/BoardView/components/Square.js
+++ src/components/Board/components/BoardView/components/Square.js
@@ -1,10 +1,11 @@
-import React, { useCallback, useMemo } from 'react';
+import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import { useDrop } from 'react-dnd';
+import { createSelector } from 'reselect';
 import isEqual from 'lodash/isEqual';
 
-import { activeKing, getPiece } from 'services/logic/fen';
+import { activeKing, getPiece, isOccupied } from 'services/logic/fen';
 import { moveToSquares } from 'services/logic/square';
 
 import { getIsMoving, getSelectedMove, getTargets } from 'reducers';
@@ -18,12 +19,69 @@ import KingInCheckIndicator from './KingInCheckIndicator';
 import SelectionIndicator from './SelectionIndicator';
 import TargetIndicator from './TargetIndicator';
 
+const getPieceCode = (selectedMove, square) =>
+  getPiece(selectedMove.fen, square) || undefined;
+
+const getIsOccupied = (selectedMove, square) =>
+  isOccupied(selectedMove.fen, square);
+
+const getIsInLastMove = (selectedMove, square) => {
+  if (selectedMove.move === null) return false;
+
+  return moveToSquares(selectedMove.move).includes(square);
+};
+
+const getIsKingInCheck = (selectedMove, square) => {
+  if (!selectedMove.givesCheck) return false;
+
+  return activeKing(selectedMove.fen) === square;
+};
+
+const getSelectedMoveSquareDetails = createSelector(
+  state => getSelectedMove(state),
+  (_, square) => square,
+
+  (selectedMove, square) => ({
+    pieceCode: getPieceCode(selectedMove, square),
+    isOccupied: getIsOccupied(selectedMove, square),
+    isInLastMove: getIsInLastMove(selectedMove, square),
+    isKingInCheck: getIsKingInCheck(selectedMove, square),
+  }),
+);
+
+const getIsSelected = createSelector(
+  state => getIsMoving(state),
+  state => state.selectedSquare,
+  (_, square) => square,
+
+  (isMoving, selectedSquare, square) => {
+    if (isMoving) return false;
+
+    return selectedSquare === square;
+  },
+);
+
+const getIsTargeted = createSelector(
+  state => getIsMoving(state),
+  state => getTargets(state),
+  (_, square) => square,
+
+  (isMoving, targets, square) => {
+    if (isMoving) return false;
+
+    // TODO: remove index access?
+    return targets.some(move => moveToSquares(move)[1] === square);
+  },
+);
+
 // TODO make handle square click an action?
 // eslint-disable-next-line complexity
 const Square = ({ handleSquareClick, square }) => {
   const [{ isOver }, drop] = useDrop({
     accept: 'PIECE',
-    drop: () => { handleSquareClick(square)(); },
+    drop: () => {
+      handleSquareClick(square)();
+    },
     collect: monitor => ({
       isOver: !!monitor.isOver(),
       canDrop: !!monitor.canDrop(),
@@ -31,42 +89,15 @@ const Square = ({ handleSquareClick, square }) => {
   });
 
   const [moveX, moveY] = useSelector(state => state.animationOffset, isEqual);
-  const isMoving = useSelector(state => getIsMoving(state));
-  const selectedMove = useSelector(state => getSelectedMove(state), isEqual);
   const selectedSquare = useSelector(state => state.selectedSquare);
-  const targets = useSelector(state => getTargets(state), isEqual);
 
-  const pieceCode = useMemo(
-    () => getPiece(selectedMove.fen, square) || undefined,
-    [selectedMove.fen, square],
+  // Component selectors
+  const { pieceCode, isOccupied, isInLastMove, isKingInCheck } = useSelector(
+    state => getSelectedMoveSquareDetails(state, square),
+    isEqual,
   );
-
-  const isOccupied = useMemo(() => pieceCode !== undefined, [pieceCode]);
-
-  const isInLastMove = useMemo(() => {
-    if (selectedMove.move === null) return false;
-
-    return moveToSquares(selectedMove.move).includes(square);
-  }, [selectedMove.move, square]);
-
-  const isKingInCheck = useMemo(() => {
-    if (!selectedMove.givesCheck) return false;
-
-    return activeKing(selectedMove.fen) === square;
-  }, [selectedMove.fen, selectedMove.givesCheck, square]);
-
-  const isSelected = useMemo(() => {
-    if (isMoving) return false;
-
-    return selectedSquare === square;
-  }, [isMoving, selectedSquare, square]);
-
-  const isTargeted = useMemo(() => {
-    if (isMoving) return false;
-
-    // TODO: remove index access?
-    return targets.some(move => moveToSquares(move)[1] === square);
-  }, [isMoving, square, targets]);
+  const isSelected = useSelector(state => getIsSelected(state, square));
+  const isTargeted = useSelector(state => getIsTargeted(state, square));
 
   const renderTargetIndicator = useCallback(
     () => <TargetIndicator occupied={isOccupied} />,
