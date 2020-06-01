27319e789375caf9edc1a881f97acfb71af5d7f8
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-06-01 06:16:03 -0400

    update make move logic

--- src/actions/makeMove.js
+++ src/actions/makeMove.js
@@ -2,7 +2,7 @@ import actions from 'actions';
 
 const makeMove = move => dispatch => {
   dispatch(actions.game.moves.add(move));
-  dispatch(actions.game.selectedFen.set(null));
+  dispatch(actions.game.selectedFen.set(move.fen));
   dispatch(actions.game.showConfirmMoveMenu.set(true));
 };
 
