1cb7e8327a2b6d4e6bdc41f1d623b707b6e15101
maximpertsov <maxim.pertsov@gmail.com>
2020-07-01 21:51:58 -0400

    Give board a background color (#122)
    * give board a background color
    
    * update snapshots

--- src/components/Board/components/BoardView/components/LastMoveIndicator.js
+++ src/components/Board/components/BoardView/components/LastMoveIndicator.js
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 import { fillParentElement } from 'commonStyles';
 
-const LAST_MOVE_COLOR = 'rgba(201,255,229,1.0)';
+const LAST_MOVE_COLOR = 'rgba(201,255,229,0.8)';
 
 const LastMoveIndicator = styled.div({
   backgroundColor: LAST_MOVE_COLOR,
