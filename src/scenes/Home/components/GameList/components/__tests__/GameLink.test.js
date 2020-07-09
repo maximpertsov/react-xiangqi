d741bc30bb2215fca02ca88fba655eeeed0cffda
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-07-08 21:21:51 -0400

    test game link

--- src/scenes/Home/components/GameList/components/__tests__/GameLink.test.js
+++ src/scenes/Home/components/GameList/components/__tests__/GameLink.test.js
@@ -1,2 +1,37 @@
-test.todo('render game link');
-test.todo('click game link');
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import actions from 'actions';
+
+import GameLink from '../GameLink';
+
+jest.mock('react-redux');
+
+describe('GameLink', () => {
+  const slug = 'abc123';
+  const store = mockStore({});
+
+  let wrapper;
+
+  beforeEach(() => {
+    useDispatch.mockReturnValue(store.dispatch);
+    wrapper = shallowWrappedComponent(<GameLink slug={slug} />, store);
+  });
+
+  afterEach(() => {
+    store.getActions();
+    jest.resetAllMocks();
+  });
+
+  test('snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test('click', () => {
+    wrapper.find('Button').simulate('click');
+
+    expect(store.getActions()).toStrictEqual([
+      actions.game.slug.set(slug),
+      actions.home.showGame.set(true),
+    ]);
+  });
+});
