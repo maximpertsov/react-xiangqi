262f7f0670fd697e7300650517ee39ac9f2c3223
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-07-10 01:53:54 -0400

    test clicks
    source: https://stackoverflow.com/questions/45849936/how-to-use-enzyme-shallowwrapper-to-find-a-react-component-stored-as-a-prop-in-a/60742285#60742285

--- src/scenes/Home/components/__tests__/NewGameMenu.test.js
+++ src/scenes/Home/components/__tests__/NewGameMenu.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import axios from 'axios';
 
 import NewGameMenu from '../NewGameMenu';
 
 jest.mock('react-redux');
+jest.mock('axios');
 
 describe('NewGameMenu', () => {
   let store;
@@ -15,20 +17,38 @@ describe('NewGameMenu', () => {
   });
 
   afterEach(() => {
-    store.getActions();
+    store.clearActions();
     jest.resetAllMocks();
   });
 
   describe('did not make game request', () => {
     beforeAll(() => {
       store = mockStore({
-        lobbyGames: [],
+        lobbyGames: [{ id: 123, player1: 'bob', parameters: {} }],
         username: 'alice',
       });
     });
 
+    beforeEach(() => {
+      axios.post.mockResolvedValue({});
+    });
+
     test('snapshot', () => {
       expect(wrapper).toMatchSnapshot();
+    });
+
+    test('click', () => {
+      expect.assertions(1);
+      const button = wrapper.wrap(
+        wrapper
+          .find('Popup')
+          .first()
+          .prop('trigger'),
+      );
+      button.simulate('click');
+
+      // TODO: called with?
+      expect(axios.post).toHaveBeenCalled();
     });
   });
 
@@ -40,8 +60,20 @@ describe('NewGameMenu', () => {
       });
     });
 
+    beforeEach(() => {
+      axios.delete.mockResolvedValue({});
+    });
+
     test('snapshot', () => {
       expect(wrapper).toMatchSnapshot();
+    });
+
+    test('click', () => {
+      expect.assertions(1);
+      wrapper.find('Button').simulate('click');
+
+      // TODO: called with?
+      expect(axios.delete).toHaveBeenCalled();
     });
   });
 });
