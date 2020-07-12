c629b6d1989a690f62dcb581503e46ebada363a4
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-07-12 11:18:02 -0400

    move DND provider up to app

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import 'semantic-ui-css/semantic.min.css';
 import React from 'react';
+import { DndProvider } from 'react-dnd';
+import Backend from 'react-dnd-html5-backend';
 
 import Home from './scenes/Home';
 
-const App = () => <Home />;
+const App = () => (
+  <DndProvider backend={Backend}>
+    <Home />
+  </DndProvider>
+);
 
 export default App;
