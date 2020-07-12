5d888d2d63dba550e54d5ec88336a63ce028723f
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-07-12 11:26:47 -0400

    Move styles to app component def

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
-import styled from '@emotion/styled';
 import WebSocketProvider from 'services/WebSocketProvider';
 
 import App from './App';
@@ -22,29 +21,10 @@ const store = compose(
   getReduxDevExtOptions(),
 )(createStore)(rootReducer);
 
-const Wrapper = styled.div`
-  body {
-    margin: 0;
-    padding: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
-      'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
-      'Helvetica Neue', sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-
-  code {
-    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-      monospace;
-  }
-`;
-
 ReactDOM.render(
   <Provider store={store}>
     <WebSocketProvider>
-      <Wrapper>
-        <App />
-      </Wrapper>
+      <App />
     </WebSocketProvider>
   </Provider>,
   document.getElementById('root'),
