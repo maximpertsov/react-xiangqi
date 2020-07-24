0b5f5b21e19d97c0a7d6db7afd164bba4396412d
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-07-24 17:20:05 -0400

    bottom

--- src/components/Board/components/BoardView/images.js
+++ src/components/Board/components/BoardView/images.js
@@ -1,19 +1,24 @@
+import flatten from 'lodash/flatten';
+
+import bottom from './assets/square-bottom.svg';
 import topLeft from './assets/square-top-left.svg';
 import topRight from './assets/square-top-right.svg';
 import top from './assets/square-top.svg';
 
 const imageByIndex = Object.freeze([
-  topLeft,
-  top,
-  top,
-  null,
-  null,
-  null,
-  top,
-  top,
-  topRight,
+  [topLeft, top, top, null, null, null, top, top, topRight],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, bottom, bottom, bottom, bottom, bottom, bottom, bottom, null],
+  //
+  [topLeft, top, top, top, top, top, top, top, topRight],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, bottom, bottom, null, null, null, bottom, bottom, null],
 ]);
 
-const getImageByIndex = i => imageByIndex[i];
+const getImageByIndex = i => flatten(imageByIndex)[i];
 
 export default getImageByIndex;
