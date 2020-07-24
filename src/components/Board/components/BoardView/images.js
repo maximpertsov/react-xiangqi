5d54648d8a724563dec43784925949f1f785211c
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-07-24 18:00:22 -0400

    palace...

--- src/components/Board/components/BoardView/images.js
+++ src/components/Board/components/BoardView/images.js
@@ -1,27 +1,28 @@
 import flatten from 'lodash/flatten';
 
-import bottomLeft from './assets/square-bottom-left.svg';
-import bottomRight from './assets/square-bottom-right.svg';
-import bottom from './assets/square-bottom.svg';
+import botLeft from './assets/square-bottom-left.svg';
+import botRight from './assets/square-bottom-right.svg';
+import bot from './assets/square-bottom.svg';
 import inner from './assets/square-inner.svg';
 import left from './assets/square-left.svg';
+import lpTopLeft from './assets/square-lower-palace-top-left.svg';
+import pCenter from './assets/square-palace-center.svg';
 import right from './assets/square-right.svg';
 import topLeft from './assets/square-top-left.svg';
 import topRight from './assets/square-top-right.svg';
 import top from './assets/square-top.svg';
 
 const imageByIndex = Object.freeze([
-  [topLeft, top, top, null, null, null, top, top, topRight],
-  [left, inner, inner, null, null, null, inner, inner, right],
-  [left, inner, inner, null, null, null, inner, inner, right],
+  [topLeft, top, top, lpTopLeft, null, null, top, top, topRight],
+  [left, inner, inner, inner, pCenter, inner, inner, inner, right],
+  [left, inner, inner, null, inner, null, inner, inner, right],
   [left, inner, inner, inner, inner, inner, inner, inner, right],
-  [bottomLeft, bottom, bottom, bottom, bottom, bottom, bottom, bottom, bottomRight],
-  //
+  [botLeft, bot, bot, bot, bot, bot, bot, bot, botRight],
   [topLeft, top, top, top, top, top, top, top, topRight],
   [left, inner, inner, inner, inner, inner, inner, inner, right],
-  [left, inner, inner, null, null, null, inner, inner, right],
-  [left, inner, inner, null, null, null, inner, inner, right],
-  [bottomLeft, bottom, bottom, null, null, null, bottom, bottom, bottomRight],
+  [left, inner, inner, null, inner, null, inner, inner, right],
+  [left, inner, inner, inner, pCenter, inner, inner, inner, right],
+  [botLeft, bot, bot, null, null, null, bot, bot, botRight],
 ]);
 
 const getImageByIndex = i => flatten(imageByIndex)[i];
