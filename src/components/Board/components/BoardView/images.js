c5b7a5118471410aec9ed0e0c3d600bffe01f904
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-07-24 18:40:28 -0400

    upper palace done

--- src/components/Board/components/BoardView/images.js
+++ src/components/Board/components/BoardView/images.js
@@ -5,24 +5,27 @@ import botRight from './assets/square-bottom-right.svg';
 import bot from './assets/square-bottom.svg';
 import inner from './assets/square-inner.svg';
 import left from './assets/square-left.svg';
-import lpTopLeft from './assets/square-lower-palace-top-left.svg';
 import pCenter from './assets/square-palace-center.svg';
 import right from './assets/square-right.svg';
 import topLeft from './assets/square-top-left.svg';
 import topRight from './assets/square-top-right.svg';
 import top from './assets/square-top.svg';
+import upBotLeft from './assets/square-upper-palace-bottom-left.svg';
+import upBotRight from './assets/square-upper-palace-bottom-right.svg';
+import upTopLeft from './assets/square-upper-palace-top-left.svg';
+import upTopRight from './assets/square-upper-palace-top-right.svg';
 
 const imageByIndex = Object.freeze([
-  [topLeft, top, top, lpTopLeft, null, null, top, top, topRight],
+  [topLeft, top, top, upTopLeft, top, upTopRight, top, top, topRight],
   [left, inner, inner, inner, pCenter, inner, inner, inner, right],
-  [left, inner, inner, null, inner, null, inner, inner, right],
+  [left, inner, inner, upBotLeft, inner, upBotRight, inner, inner, right],
   [left, inner, inner, inner, inner, inner, inner, inner, right],
   [botLeft, bot, bot, bot, bot, bot, bot, bot, botRight],
   [topLeft, top, top, top, top, top, top, top, topRight],
   [left, inner, inner, inner, inner, inner, inner, inner, right],
   [left, inner, inner, null, inner, null, inner, inner, right],
   [left, inner, inner, inner, pCenter, inner, inner, inner, right],
-  [botLeft, bot, bot, null, null, null, bot, bot, botRight],
+  [botLeft, bot, bot, null, bot, null, bot, bot, botRight],
 ]);
 
 const getImageByIndex = i => flatten(imageByIndex)[i];
