c1ceaf7519e364d7472a3bffb95e27e9207eea64
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-07-24 17:28:22 -0400

    left-right

--- src/components/Board/components/BoardView/images.js
+++ src/components/Board/components/BoardView/images.js
@@ -2,21 +2,23 @@ import flatten from 'lodash/flatten';
 
 import bottom from './assets/square-bottom.svg';
 import inner from './assets/square-inner.svg';
+import left from './assets/square-left.svg';
+import right from './assets/square-right.svg';
 import topLeft from './assets/square-top-left.svg';
 import topRight from './assets/square-top-right.svg';
 import top from './assets/square-top.svg';
 
 const imageByIndex = Object.freeze([
   [topLeft, top, top, null, null, null, top, top, topRight],
-  [null, inner, inner, null, null, null, inner, inner, null],
-  [null, inner, inner, null, null, null, inner, inner, null],
-  [null, inner, inner, inner, inner, inner, inner, inner, null],
+  [left, inner, inner, null, null, null, inner, inner, right],
+  [left, inner, inner, null, null, null, inner, inner, right],
+  [left, inner, inner, inner, inner, inner, inner, inner, right],
   [null, bottom, bottom, bottom, bottom, bottom, bottom, bottom, null],
   //
   [topLeft, top, top, top, top, top, top, top, topRight],
-  [null, inner, inner, inner, inner, inner, inner, inner, null],
-  [null, inner, inner, null, null, null, inner, inner, null],
-  [null, inner, inner, null, null, null, inner, inner, null],
+  [left, inner, inner, inner, inner, inner, inner, inner, right],
+  [left, inner, inner, null, null, null, inner, inner, right],
+  [left, inner, inner, null, null, null, inner, inner, right],
   [null, bottom, bottom, null, null, null, bottom, bottom, null],
 ]);
 
