4dda9a311e50fb05fc245198cfcbce547ed96e4b
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-07-25 02:17:59 -0400

    selection indicator

--- src/components/Board/components/BoardView/components/SelectionIndicator.js
+++ src/components/Board/components/BoardView/components/SelectionIndicator.js
@@ -1,28 +1,8 @@
-import React, { useContext } from 'react';
-import { useSelector } from 'react-redux';
-import { createSelector } from 'reselect';
+import React from 'react';
 import styled from '@emotion/styled';
-import isEqual from 'lodash/isEqual';
 
 import { fillParentElement, SELECTION_COLOR } from 'commonStyles';
-import { SquareContext } from 'contexts/SquareProvider';
-import { getIsMoving } from 'reducers';
-
-const getIsSelected = ({ isMoving, selectedSquare, square }) => {
-  if (isMoving) return false;
-
-  return selectedSquare === square;
-};
-
-const mapStateToProps = createSelector(
-  state => getIsMoving(state),
-  state => state.selectedSquare,
-  (_, props) => props.square,
-
-  (isMoving, selectedSquare, square) => ({
-    isSelected: getIsSelected({ isMoving, selectedSquare, square }),
-  }),
-);
+import { useSquareContext } from 'contexts/SquareProvider';
 
 const Wrapper = styled.div({
   backgroundColor: SELECTION_COLOR,
@@ -31,11 +11,7 @@ const Wrapper = styled.div({
 });
 
 const SelectionIndicator = () => {
-  const { square } = useContext(SquareContext);
-  const { isSelected } = useSelector(
-    state => mapStateToProps(state, { square }),
-    isEqual,
-  );
+  const { isSelected } = useSquareContext();
   return isSelected && <Wrapper className="SelectionIndicator" />;
 };
 
