ab8ab69d540a9d7a2014baf055b7a570c1bb69aa
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-07-25 02:42:56 -0400

    move square-specific logic from board to square

--- src/components/Board/components/BoardView/components/Square.js
+++ src/components/Board/components/BoardView/components/Square.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { useDrop } from 'react-dnd';
-import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { createSelector } from 'reselect';
+import get from 'lodash/get';
+import isEqual from 'lodash/isEqual';
 
+import actions from 'actions';
+import animateMove from 'actions/animateMove';
+import makeMove from 'actions/makeMove';
 import { useSquareContext } from 'contexts/SquareProvider';
+import { getBottomPlayerIsRed, getLegalMoves } from 'reducers';
+import { isOccupied, sameTeam } from 'services/logic/fen';
+import { squaresToUci } from 'services/logic/square';
 
 import DropIndicator from './DropIndicator';
 import KingInCheckIndicator from './KingInCheckIndicator';
@@ -12,13 +21,70 @@ import SelectionIndicator from './SelectionIndicator';
 import SquareView from './SquareView';
 import TargetIndicator from './TargetIndicator';
 
+const ANIMATION_DELAY = 150;
+
+const mapStateToProps = createSelector(
+  [state => state],
+
+  state => ({
+    bottomPlayerIsRed: getBottomPlayerIsRed(state),
+    legalMoves: getLegalMoves(state),
+    selectedFen: state.selectedFen,
+    selectedSquare: state.selectedSquare,
+  }),
+);
+
 // TODO make handle square click an action?
-const Square = ({ handleSquareClick }) => {
+const Square = () => {
+  const dispatch = useDispatch();
   const { square } = useSquareContext();
+  const {
+    bottomPlayerIsRed,
+    legalMoves,
+    selectedFen,
+    selectedSquare,
+  } = useSelector(mapStateToProps, isEqual);
+
+  const selectedCanCapture = () => {
+    if (selectedSquare === null) return false;
+    if (!isOccupied(selectedFen, selectedSquare)) return false;
+    if (!isOccupied(selectedFen, square)) return false;
+
+    return !sameTeam(selectedFen, square, selectedSquare);
+  };
+
+  const legalFen = uci => get(legalMoves, uci, false);
+
+  const handleMove = uci => {
+    const fen = legalFen(uci);
+    if (fen) {
+      dispatch(animateMove({ bottomPlayerIsRed, uci }));
+      setTimeout(() => {
+        dispatch(makeMove({ fen, uci }));
+        dispatch(actions.board.animationOffset.clear());
+        dispatch(actions.board.selectedSquare.set(null));
+      }, ANIMATION_DELAY);
+    } else {
+      dispatch(actions.board.selectedSquare.set(null));
+    }
+  };
+
+  const handleSquareClick = () => {
+    if (square === selectedSquare) {
+      dispatch(actions.board.selectedSquare.set(null));
+    } else if (isOccupied(selectedFen, square) && !selectedCanCapture()) {
+      dispatch(actions.board.selectedSquare.set(square));
+    } else if (selectedSquare !== null) {
+      handleMove(squaresToUci(selectedSquare, square));
+    } else {
+      dispatch(actions.board.selectedSquare.set(null));
+    }
+  };
+
   const [{ isOver }, drop] = useDrop({
     accept: 'PIECE',
     drop: () => {
-      handleSquareClick(square)();
+      handleSquareClick();
     },
     collect: monitor => ({
       isOver: !!monitor.isOver(),
@@ -27,7 +93,7 @@ const Square = ({ handleSquareClick }) => {
   });
 
   return (
-    <SquareView handleClick={handleSquareClick(square)} ref={drop}>
+    <SquareView handleClick={handleSquareClick} ref={drop}>
       <Piece />
       {isOver && <DropIndicator />}
       <LastMoveIndicator />
@@ -36,10 +102,6 @@ const Square = ({ handleSquareClick }) => {
       <TargetIndicator />
     </SquareView>
   );
-};
-
-Square.propTypes = {
-  handleSquareClick: PropTypes.func.isRequired,
 };
 
 export default Square;
--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,87 +1,31 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { createSelector } from 'reselect';
-import get from 'lodash/get';
 import isEqual from 'lodash/isEqual';
 
 import actions from 'actions';
-import animateMove from 'actions/animateMove';
-import makeMove from 'actions/makeMove';
-import { getBottomPlayerIsRed, getLegalMoves } from 'reducers';
-import { isOccupied, sameTeam } from 'services/logic/fen';
-import { squaresToUci } from 'services/logic/square';
+import { getBottomPlayerIsRed } from 'reducers';
 
 import BoardView from './components/BoardView';
 
-const ANIMATION_DELAY = 150;
-
 const mapStateToProps = createSelector(
-  [state => state],
+  state => state,
 
   state => ({
     bottomPlayerIsRed: getBottomPlayerIsRed(state),
-    legalMoves: getLegalMoves(state),
-    selectedFen: state.selectedFen,
-    selectedSquare: state.selectedSquare,
   }),
 );
 
 const Board = () => {
   const dispatch = useDispatch();
 
-  const {
-    bottomPlayerIsRed,
-    legalMoves,
-    selectedFen,
-    selectedSquare,
-  } = useSelector(mapStateToProps, isEqual);
+  const { bottomPlayerIsRed } = useSelector(mapStateToProps, isEqual);
 
   useEffect(() => {
     dispatch(actions.board.selectedSquare.set(null));
-  }, [dispatch, selectedFen]);
+  }, [dispatch]);
 
-  const selectedCanCapture = square => {
-    if (selectedSquare === null) return false;
-    if (!isOccupied(selectedFen, selectedSquare)) return false;
-    if (!isOccupied(selectedFen, square)) return false;
-
-    return !sameTeam(selectedFen, square, selectedSquare);
-  };
-
-  const legalFen = uci => get(legalMoves, uci, false);
-
-  const handleMove = uci => {
-    const fen = legalFen(uci);
-    if (fen) {
-      dispatch(animateMove({ bottomPlayerIsRed, uci }));
-      setTimeout(() => {
-        dispatch(makeMove({ fen, uci }));
-        dispatch(actions.board.animationOffset.clear());
-        dispatch(actions.board.selectedSquare.set(null));
-      }, ANIMATION_DELAY);
-    } else {
-      dispatch(actions.board.selectedSquare.set(null));
-    }
-  };
-
-  const handleSquareClick = square => () => {
-    if (square === selectedSquare) {
-      dispatch(actions.board.selectedSquare.set(null));
-    } else if (isOccupied(selectedFen, square) && !selectedCanCapture(square)) {
-      dispatch(actions.board.selectedSquare.set(square));
-    } else if (selectedSquare !== null) {
-      handleMove(squaresToUci(selectedSquare, square));
-    } else {
-      dispatch(actions.board.selectedSquare.set(null));
-    }
-  };
-
-  return (
-    <BoardView
-      teamBlackPOV={!bottomPlayerIsRed}
-      handleSquareClick={handleSquareClick}
-    />
-  );
+  return <BoardView teamBlackPOV={!bottomPlayerIsRed} />;
 };
 
 export default Board;
